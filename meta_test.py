85c1ecbdeda495ae15ef656e63516d79ef335241
Colm Ryan <cryan@bbn.com>
2015-09-14 12:06:58 -0400

    :fire: old Instrument class and testing script

--- meta_test.py
+++
@@ -1,9 +0,0 @@
-from instruments.instrument import Instrument, Command
-
-class AgilentWhatever(Instrument):
-	power = Command("power", get_string=":pow?", set_string=":pow %g dbm;")
-	frequency = Command("frequency", get_string=":freq?", set_string=":freq %g Hz;")
-	current = Command("current", get_string=":curr?")
-
-
-uwSource = AgilentWhatever()
