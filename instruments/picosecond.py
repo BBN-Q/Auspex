d2981d1aaa3434c1c87e3bca3096e7a73f1f2ff5
CoherenceController <qlab@bbn.com>
2015-09-17 11:58:35 -0400

    :art: Formatting changes, slight fixes
    --GER

--- instruments/picosecond.py
+++ instruments/picosecond.py
@@ -2,27 +2,30 @@ from .instrument import Instrument, Command, FloatCommand
 
 class Picosecond10070A(Instrument):
     """Picosecond 10070A Pulser"""
-    amplitude = Command("amplitude", get_string="amplitude?", set_string="amplitude {:g}")
-    delay = FloatCommand("delay", get_string="delay?", set_string="delay {:g}")
-    duration = FloatCommand("duration", get_string="duration?", set_string="duration {:g}")
-    level = FloatCommand("level", get_string="level?", set_string="level {:g}")
-    period = FloatCommand("period", get_string="period?", set_string="period {:g}")
+    amplitude = FloatCommand("amplitude", get_string="amplitude?", set_string="amplitude {:g}")
+    delay     = FloatCommand("delay", get_string="delay?", set_string="delay {:g}")
+    duration  = FloatCommand("duration", get_string="duration?", set_string="duration {:g}")
+    level     = FloatCommand("level", get_string="level?", set_string="level {:g}")
+    period    = FloatCommand("period", get_string="period?", set_string="period {:g}")
     frequency = FloatCommand("frequency", get_string="frequency?", set_string="frequency {:g}")
-    offset = FloatCommand("offset", get_string="offset?", set_string="offset {:g}")
-    output = FloatCommand("offset", get_string="offset?", set_string="offset {:g}")
+    offset    = FloatCommand("offset", get_string="offset?", set_string="offset {:g}")
 
-    def __init__(self, resourceName, *args, **kwargs):
-        super(Picosecond10070A, self).__init__(resourceName, "Picosecond 10070A Pulser", *args, **kwargs)
-        self.write("header off")
-        self.write("trigger GPIB")
+    def __init__(self, name, resource_name, *args, **kwargs):
+        super(Picosecond10070A, self).__init__(name, resource_name, *args, **kwargs)
+        self.interface.write("header off")
+        self.interface.write("trigger GPIB")
+        self.interface._instrument.read_termination = u"\n"
 
     # This command is syntactically screwy
     @property
     def output(self):
-        return self.ask("enable?") == "YES"
+        return self.interface.query("enable?") == "YES"
     @output.setter
     def output(self, value):
         if value:
-            self.write("enable")
+            self.interface.write("enable")
         else:
-            self.write("disable")
+            self.interface.write("disable")
+
+    def trigger(self):
+        self.interface.write("*TRG")
