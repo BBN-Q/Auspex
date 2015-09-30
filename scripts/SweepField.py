78d490d7fa4216e92ee6c3ca83c638eaee07d06a
CoherenceController <qlab@bbn.com>
2015-09-30 10:30:44 -0400

    Field sweeping script now makes use of the Hall probe
    -- GER and CAR

--- scripts/SweepField.py
+++ scripts/SweepField.py
@@ -9,16 +9,21 @@ import pandas as pd
 
 from instruments.kepco import BOP2020M
 from instruments.stanford import SR830
+from instruments.magnet import Electromagnet
+from instruments.hall_probe import HallProbe
 from sweep import Sweep
 from procedure import FloatParameter, Quantity, Procedure
 
 
 class FieldTest(Procedure):
-    current = FloatParameter("Supply Current", unit="A")
+    set_field = FloatParameter("Set Field", unit="G")
+    field   = Quantity("Field", unit="G")
     voltage = Quantity("Magnitude", unit="V")
 
-    bop = BOP2020M("Kepco Power Supply", "GPIB1::1::INSTR")
+    bop  = BOP2020M("Kepco Power Supply", "GPIB1::1::INSTR")
     lock = SR830("Lockin Amplifier", "GPIB1::9::INSTR")
+    hp   = HallProbe("calibration/HallProbe.cal", lock.set_ao1, lock.get_ai1)
+    mag  = Electromagnet('calibration/GMW.cal', hp.get_field, bop.set_current, bop.get_current)
 
     def instruments_init(self):
         self.tc_delay = 9*self.lock.tc
@@ -29,7 +34,8 @@ class FieldTest(Procedure):
             time.sleep(self.tc_delay)
             return np.mean( [self.lock.r for i in range(self.averages)] )
 
-        self.current.set_method(self.bop.set_current)
+        self.set_field.set_method(self.mag.set_field)
+        self.field.set_method(self.mag.get_field)
         self.voltage.set_method(lockin_measure)
 
     def run(self):
@@ -48,15 +54,15 @@ if __name__ == '__main__':
 
     # Define a sweep over prarameters
     sw = Sweep(proc)
-    values = np.append(np.arange(-2, 10.1, 0.5), np.arange(10.0,-2.1,-0.5)).tolist()
-    sw.add_parameter(proc.current, values)
+    values = np.append(np.arange(-1100, 1100.1, 25), np.arange(1100.0,-1100.1,-25)).tolist()
+    sw.add_parameter(proc.set_field, values)
 
     # Define a writer
-    sw.add_writer('FieldLoops.h5', 'MinorLoop', proc.voltage)
+    sw.add_writer('data/FieldLoops.h5', 'SWS2129(2,0)G-(011,09)', 'MajorLoop', proc.field, proc.voltage)
 
     proc.instruments_init()
     for i in sw:
-        logging.info("Current, Lockin Magnitude: {:f}, {:g}".format(proc.current.value, proc.voltage.value) )
+        logging.info("Field, Lockin Magnitude: {:f}, {:g}".format(proc.field.value, proc.voltage.value) )
     proc.instruments_shutdown()
 
     # proc.current.value = 0.0
