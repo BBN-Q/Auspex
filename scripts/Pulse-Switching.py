a8f03ca977bed654ebce2b19495089b385f4b187
CoherenceController <qlab@bbn.com>
2015-10-02 16:47:42 -0400

    Updated pulse switching for new style sweeps
    --GER

--- scripts/Pulse-Switching.py
+++ scripts/Pulse-Switching.py
@@ -1,37 +1,37 @@
 from __future__ import print_function, division
 import time
 import logging
-logging.basicConfig(format='%(levelname)s:\t%(message)s', level=logging.INFO)
+# logging.basicConfig(format='%(levelname)s:\t%(message)s', level=logging.INFO)
 
 import numpy as np
-import scipy as sp
 import pandas as pd
 
 from instruments.kepco import BOP2020M
+from instruments.magnet import Electromagnet
+from instruments.hall_probe import HallProbe
 from instruments.stanford import SR830
 from instruments.picosecond import Picosecond10070A
 from sweep import Sweep
 from procedure import FloatParameter, IntParameter, Quantity, Procedure
 
-import ipdb
-
 class Switching(Procedure):
-    pulse_voltage = FloatParameter("Pulse Amplitude", unit="A")
+    pulse_voltage  = FloatParameter("Pulse Amplitude", unit="V")
     pulse_duration = FloatParameter("Pulse Duration", unit="s")
     attempt_number = IntParameter("Switching Attempt Index")
-    supply_current = FloatParameter("Supply Current", unit="A")
-    
-    initial_state = Quantity("Initial Voltage State", unit="V")
-    final_state = Quantity("Final Voltage State", unit="V")
 
-    bop = BOP2020M("Kepco Power Supply", "GPIB1::1::INSTR")
+    field         = Quantity("Field", unit="G")
+    initial_state = Quantity("Initial Voltage", unit="V")
+    final_state   = Quantity("Final Voltage", unit="V")
+
+    bop  = BOP2020M("Kepco Power Supply", "GPIB1::1::INSTR")
     lock = SR830("Lockin Amplifier", "GPIB1::9::INSTR")
+    hp   = HallProbe("calibration/HallProbe.cal", lock.set_ao1, lock.get_ai1)
+    mag  = Electromagnet('calibration/GMW.cal', hp.get_field, bop.set_current, bop.get_current)
     pspl = Picosecond10070A("Pulse Generator", "GPIB1::24::INSTR")
 
     def instruments_init(self):
         self.tc_delay = 9*self.lock.tc
         self.averages = 2
-        self.bop.output = True
         self.pspl.output = True
 
         def lockin_measure_initial():
@@ -43,13 +43,13 @@ class Switching(Procedure):
             time.sleep(self.tc_delay)
             return np.mean( [self.lock.r for i in range(self.averages)] )
 
-        self.supply_current.set_method(self.bop.set_current)
-        self.initial_state.set_method(lockin_measure_initial)
-        self.final_state.set_method(lockin_measure_final)
+        self.field.assign_method(self.mag.set_field)
+        self.initial_state.assign_method(lockin_measure_initial)
+        self.final_state.assign_method(lockin_measure_final)
 
-        self.pulse_voltage.set_method(self.pspl.set_amplitude)
-        self.pulse_duration.set_method(self.pspl.set_duration)
-        self.attempt_number.set_method(lambda x: 42)
+        self.pulse_voltage.assign_method(self.pspl.set_amplitude)
+        self.pulse_duration.assign_method(self.pspl.set_duration)
+        self.attempt_number.assign_method(lambda x: 42)
 
     def run(self):
         """This is run for each step in a sweep."""
@@ -61,27 +61,27 @@ class Switching(Procedure):
 
     def instruments_shutdown(self):
         self.bop.current = 0.0
-        self.bop.output = False
         self.pspl.output = False
 
 if __name__ == '__main__':
 
     proc = Switching()
-    proc.supply_current.value = 4.0
+    proc.field.value = -405
+    proc.pulse_duration.value = 0.2e-9
 
     # Define a sweep over prarameters
     sw = Sweep(proc)
-    sw.add_parameter(proc.pulse_voltage, -7.5*np.power(10,-np.arange(9,8,-1)/20.0))
-    sw.add_parameter(proc.pulse_duration, np.arange(0.2e-9, 0.31e-9, 0.1e-9))
+    sw.add_parameter(proc.pulse_voltage, -7.5*np.power(10,-np.arange(4,3,-1)/20.0))
+    # sw.add_parameter(proc.pulse_duration, np.arange(0.2e-9, 0.31e-9, 0.1e-9))
     sw.add_parameter(proc.attempt_number, np.arange(0,50))
 
     # Define a writer
-    sw.add_writer('SwitchingAttempts.h5', 'AmplitudeSearch-Neg-8', proc.initial_state, proc.final_state)
+    sw.add_writer('data/SwitchingAttempts.h5', 'SWS2129(2,0)G-(011,09)', 'SwitchingTest', proc.initial_state, proc.final_state)
+    sw.add_plotter('Intial Voltage vs. Attempt number', proc.attempt_number, proc.initial_state, color="firebrick", line_width=2)
+    sw.add_plotter('Final Voltage vs. Attempt number', proc.attempt_number, proc.final_state, color="navy", line_width=2)
+    sw.add_plotter('Pulse Voltage', proc.pulse_voltage, proc.attempt_number, color="green", line_width=2)
 
     proc.instruments_init()
-    for i in sw:
-        logging.info("Voltage, Duration, Initial V, Final V: {:g}, {:g}, {:g}, {:g}".format(proc.pulse_voltage.value, 
-                proc.pulse_duration.value, proc.initial_state.value, proc.final_state.value) )
-    
+    sw.run()
     proc.instruments_shutdown()
     
