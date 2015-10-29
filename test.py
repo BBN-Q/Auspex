cabe3c394e789eac571bd8b7e68faafd294b7a13
Graham Rowlands <grahamrow@gmail.com>
2015-10-29 17:45:32 -0400

    Added 2D plotting capabilities

--- test.py
+++ test.py
@@ -35,8 +35,8 @@ class TestProcedure(Procedure):
     resistance_long = Quantity("Longitudinal Resistance", unit="Ohm")
 
     def init_instruments(self):
-        self.field_x.assign_method(lambda x: time.sleep(0.1))
-        self.field_y.assign_method(lambda x: time.sleep(0.05))
+        self.field_x.assign_method(lambda x: time.sleep(0.001))
+        self.field_y.assign_method(lambda x: time.sleep(0.001))
         self.resistance_trans.assign_method(lambda: self.field_x.value - self.field_y.value + 20*np.random.random())
         self.resistance_long.assign_method(lambda: self.field_x.value - self.field_y.value + 40*np.random.random())
 
@@ -52,13 +52,13 @@ if __name__ == '__main__':
   
     # Define a sweep over prarameters
     sweep1 = Sweep(proc)
-    sweep1.add_parameter(proc.field_y, np.arange(-100, 101, 10))
-    sweep1.add_parameter(proc.field_x, np.arange(-100, 101, 10))
+    field_x = sweep1.add_parameter(proc.field_y, np.arange(-100, 101, 1))
+    field_y = sweep1.add_parameter(proc.field_x, np.arange(-100, 101, 1))
     
-    p1 = sweep1.add_plotter("ResistanceL Vs Field", proc.field_x, proc.resistance_long, color="firebrick", line_width=2)
-    p2 = sweep1.add_plotter("ResistanceT Vs Field", proc.field_x, proc.resistance_trans, color="navy", line_width=2)
-
-    proc.field_y.add_post_push_hook(p1.clear)
+    plot1 = sweep1.add_plotter("ResistanceL Vs Field", proc.field_x, proc.resistance_long, color="firebrick", line_width=2)
+    # p2 = sweep1.add_plotter("ResistanceT Vs Field", proc.field_x, proc.resistance_trans, color="navy", line_width=2)
+    plot2 = sweep1.add_plotter2d("A Whole New Dimension", field_x, field_y, proc.resistance_trans, palette="Spectral11")
+    proc.field_y.add_post_push_hook(plot1.clear)
 
     sweep1.run()
 
