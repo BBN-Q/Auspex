8a72cba0dd1c223900ea866c6e63dcb4ac5ccf4b
Colm Ryan <cryan@bbn.com>
2015-11-20 10:55:32 -0500

    :white_check_mark: tweaks to support unittest discovery
    * discovery needs test directory to have an `__init__.py`
    * discovery finds `test*` and we don't want to run the live plotting tester yet.

--- test/test_instrument.py
+++ test/test_instrument.py
@@ -15,13 +15,13 @@ class InstrumentTestCase(unittest.TestCase):
 		self.instrument = TestInstrument("testing instrument", "DUMMY::RESOURCE")
 
 	def test_properties(self):
-		"""Check the property and setter/getter are implemented"""
+		"""Check that property and setter/getter are implemented"""
 		self.assertTrue(hasattr(self.instrument, "frequency")) #property
 		self.assertTrue(hasattr(self.instrument, "get_frequency") and callable(self.instrument.get_frequency)) #getter
 		self.assertTrue(hasattr(self.instrument, "set_frequency") and callable(self.instrument.get_frequency)) #setter
 
 	def test_getters(self):
-		"""Check the getter have only get methods"""
+		"""Check that Commands with only `get_string` have only get methods"""
 		self.assertTrue(hasattr(self.instrument, "serial_number")) #property
 		self.assertTrue(hasattr(self.instrument, "get_serial_number") and callable(self.instrument.get_frequency)) #getter
 		self.assertFalse(hasattr(self.instrument, "set_serial_number")) #setter should be deleted
