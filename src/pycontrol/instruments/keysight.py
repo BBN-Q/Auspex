b32f9161b6151c06b430e3405a6cc6bffb7f08af
CoherenceController <qlab@bbn.com>
2015-12-04 16:15:45 -0500

    Added ASCII waveform transfers to Arb. Binary not working yet.
    --GER

--- src/pycontrol/instruments/keysight.py
+++ src/pycontrol/instruments/keysight.py
@@ -1,5 +1,6 @@
 from .instrument import Instrument, Command, FloatCommand, IntCommand
 
+import logging
 import warnings
 import numpy as np
 
@@ -7,10 +8,15 @@ class M8190A(Instrument):
     """M8190A arbitrary waveform generator"""
 
     ref_source = Command("reference source", get_string=":ROSC:SOUR?", set_string="ROSC:SOUR {:s}", allowed_values=("EXTERNAL", "AXI", "INTERNAL"))
-    ref_source_freq = FloatCommand("reference source frequency", get_string=":ROSC:FREQ?", set_string=":ROSC:FREQ {:E}",value_range=(1e6, 200e6))
+    ref_source_freq = FloatCommand("reference source frequency", get_string=":ROSC:FREQ?", set_string=":ROSC:FREQ {:E}", value_range=(1e6, 200e6))
     sample_freq = FloatCommand("internal sample frequency", get_string=":FREQ:RAST?", set_string=":FREQ:RAST {:E}")
     sample_freq_ext = FloatCommand("external sample frequency", get_string=":FREQ:RAST:EXT?", set_string=":FREQ:RAST:EXT {:E}")
     sample_freq_source = Command("sample frequency source", get_string=":FREQ:RAST:SOUR?", set_string=":FREQ:RAST:SOUR", allowed_values=("INTERNAL", "EXTERNAL"))
+
+    waveform_output_mode = Command("waveform output mode", get_string=":TRAC:DWID?", set_string=":TRAC:DWID {:s}",
+                                   allowed_values=("WSPEED", "WPRECISION", "INTX3", "INTX12", "INTX24", "INT48"))
+    output1 = Command("channel 1 output", get_string=":OUTP1:NORM?", set_string=":OUTP1:NORM {:s}", value_map={False:"0", True:"1"})
+    output2 = Command("channel 2 output", get_string=":OUTP2:NORM?", set_string=":OUTP2:NORM {:s}", value_map={False:"0", True:"1"})
 
     def __init__(self, name, resource_name, *args, **kwargs):
         resource_name += "::hislip0::INSTR" #user guide recommends HiSLIP protocol
@@ -19,7 +25,10 @@ class M8190A(Instrument):
 
     def abort(self, channel):
         """Abort/stop signal generation on a channel"""
-        self.interface.write("ABORT{:d}")
+        self.interface.write(":ABORT{:d}")
+
+    def initiate(self, channel=1):
+        self.interface.write(":INIT:IMM{:d}".format(channel))
 
     def get_ref_source_available(self, source):
         """Checks whether reference source is available"""
@@ -28,8 +37,42 @@ class M8190A(Instrument):
             raise ValueError("reference source must be one of {:s}".format(str(allowed_values)))
         return self.interface.query(":ROSC:SOUR:CHEC? {:s}".format(source)) == '1'
 
+    def define_waveform(self, segment_id, length, channel=1):
+        self.interface.write(":TRAC{:d}:DEF {:d},{:d}".format(channel, segment_id, length))
+
+    def upload_waveform(self, wf_data, segment_id, channel=1, binary=True):
+        """Uploads the waveform to the device. Technically we should split the data into multiple chunks
+        if we exceed the 999999999 Bytes, i.e. 1GB SCPI transfer limit.
+        """
+        if np.dtype(np.float16).itemsize*len(wf_data) > 999999999:
+            raise ValueError("Waveform is too large for single transfer, go improve the upload_waveform() method.")
+        offset = 0
+        command_string = ":TRAC{:d}:DATA {:d},{:d},".format(channel, segment_id, offset)
+
+        if binary:
+            # Explicity set the endianess of the transfer
+            # self.interface.write(":FORMat:BORD NORM")
+            self.interface.write_binary_values(command_string, wf_data, datatype='i', is_big_endian=True)
+        else:
+            ascii_string = ",".join(["{:d}".format(val) for val in wf_data])
+            self.interface.write(command_string + ascii_string)
+
+    def delete_waveform(self, segment_id, channel=1):
+        self.interface.write(":TRAC{:d}:DEL {:d}".format(channel, segment_id))
+
+    def select_waveform(self, segment_id, channel=1):
+        self.interface.write(":TRAC{:d}:SEL {:d}".format(channel, segment_id))
+
+    def use_waveform(self, wf_data, segment_id, channel=1):
+        self.abort(channel)
+        self.delete_waveform(segment_id, channel=channel)
+        self.define_waveform(segment_id, len(wf_data), channel=channel)
+        self.upload_waveform(wf_data, segment_id, channel=channel, binary=False)
+        self.select_waveform(segment_id, channel=channel)
+        self.initiate(channel=channel)
+
     @staticmethod
-    def create_binary_wf_data(wf, sync_mkr, samp_mkr, vertical_resolution=12):
+    def create_binary_wf_data(wf, sync_mkr=None, samp_mkr=None, vertical_resolution=12):
         """Given numpy arrays of waveform and marker data convert to binary format.
         Assumes waveform data is np.float in range -1 to 1 and marker data can be cast to bool
         Binary format is waveform in MSB and and markers in LSB
@@ -42,13 +85,20 @@ class M8190A(Instrument):
 
         #convert waveform to integers
         scale_factor = 2**(vertical_resolution-1)
-        bin_data = np.int16(scale_factor*wf)
+        bin_data = np.int16((scale_factor-1)*np.array(wf))
+
+        if sync_mkr is None:
+            sync_mkr = np.zeros_like(wf, dtype=np.int16)
+
+        if samp_mkr is None:
+            samp_mkr = np.zeros_like(wf, dtype=np.int16)
 
         #clip if necessary
         if np.max(bin_data) > scale_factor-1 or np.min(bin_data) < -scale_factor:
-            warnings.warn("Clipping waveform")
+            warnings.warn("Clipping waveform. Max value: {:d} Min value: {:d}. Scale factor: {:d}.".format(np.max(bin_data), np.min(bin_data),scale_factor))
             bin_data = np.clip(bin_data, -scale_factor, scale_factor-1)
 
+        # bin_data = bin_data.byteswap()
         #shift up to the MSB
         bin_data = np.left_shift(bin_data, 4 if vertical_resolution == 12 else 2)
 
---
+++ test/test-M8190A.py
@@ -0,0 +1,26 @@
+from pycontrol.instruments.keysight import M8190A
+import numpy as np
+
+def waveform(time, delay=1.5e-9, rise_time=150e-12, fall_time=2.0e-9):
+    if time<=delay:
+        return np.exp(-(time-delay)**2/(2*rise_time**2))
+    if time>delay:
+        return np.exp(-(time-delay)/fall_time)
+
+if __name__ == '__main__':
+    arb = M8190A("Test Arb", "128.33.89.19")
+    print(arb.interface.query("*IDN?"))
+    arb.sample_freq = 12.0e9
+    arb.waveform_output_mode = "WSPEED"
+
+    times = np.arange(0, 42.6e-9, 1/12e9)
+    volts = [waveform(t, rise_time=0.00e-9, fall_time=1.0e-9) for t in times]
+
+    sync_mkr = np.zeros(len(volts), dtype=np.int16)
+    samp_mkr = np.zeros(len(volts), dtype=np.int16)
+    samp_mkr[0:128] = 1
+    sync_mkr[:128] = 1
+
+    segment_id = 1
+    wf = arb.create_binary_wf_data(volts, sync_mkr=sync_mkr, samp_mkr=samp_mkr)
+    arb.use_waveform(wf, segment_id)
