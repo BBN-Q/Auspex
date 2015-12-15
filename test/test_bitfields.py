74e0d7d0851f842e207f1d94d2026f6b20509cec
Colm Ryan <cryan@bbn.com>
2015-12-15 10:19:35 -0500

    :white_check_mark: unit-tests for bitfields

---
+++ test/test_bitfields.py
@@ -0,0 +1,51 @@
+import unittest
+
+from pycontrol.instruments.binutils import BitField, BitFieldUnion
+
+class TestBitFieldUnion(BitFieldUnion):
+    num_words  = BitField(16)
+    mode       = BitField(4)
+    read_write = BitField(1)
+    ack        = BitField(1)
+    reserved   = BitField(10)
+
+class BitFieldTestCase(unittest.TestCase):
+    """
+    Tests bitfield union setting/getting
+    """
+    def test_setter(self):
+        test_union = TestBitFieldUnion()
+        #Set a bitfield
+        test_union.ack = 1
+        #Set another field
+        self.assertEqual(test_union.packed, 1 << 21)
+        test_union.mode = 0xa
+        self.assertEqual(test_union.packed, (1 << 21) | (0xa << 16))
+        #clear the first field
+        test_union.ack = 0
+        self.assertEqual(test_union.packed, 0xa << 16)
+
+    def test_getter(self):
+        test_union = TestBitFieldUnion()
+        test_union.packed = 1 << 21
+        self.assertEqual(test_union.ack, 1)
+
+    def test_setter_raise(self):
+        """setting too large a value for the field should raise"""
+        test_union = TestBitFieldUnion()
+        with self.assertRaises(ValueError):
+            test_union.mode = 0xab
+
+    def test_constructor_raise(self):
+        #trying to set both packed an another field should raise
+        with self.assertRaises(AttributeError):
+            test_union = TestBitFieldUnion(packed=3, mode=3)
+
+    def test_constructor(self):
+        test_union = TestBitFieldUnion(packed=0xbaad)
+        self.assertEqual(test_union.packed, 0xbaad)
+        test_union = TestBitFieldUnion(ack=1)
+        self.assertEqual(test_union.packed, 1 << 21)
+
+if __name__ == '__main__':
+    unittest.main()
