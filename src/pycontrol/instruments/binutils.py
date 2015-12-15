934c9439e3b1aeadc82693f733ba845cbbadb2f7
Colm Ryan <cryan@bbn.com>
2015-12-15 10:19:12 -0500

    bitfield value checking and constructor kwargs

--- src/pycontrol/instruments/binutils.py
+++ src/pycontrol/instruments/binutils.py
@@ -1,13 +1,13 @@
 import collections
 
 class BitField(object):
-    """Bit field"""
+    """Bit field in a bit field union"""
     def __init__(self, width):
         super(BitField, self).__init__()
         self.width = width
 
-class BitFieldUnion(type):
-    """Painless bit packing"""
+class BitFieldUnionMeta(type):
+    """Metaclass for injecting bitfield descriptors"""
     @classmethod
     def __prepare__(self, name, bases):
         return collections.OrderedDict()
@@ -26,7 +26,20 @@ class BitFieldUnion(type):
                 def fget(self, offset=init_offset, width=v.width):
                     return (self.packed >> offset) & (2**width-1)
                 def fset(self, val, offset=init_offset, width=v.width):
+                    #check we don't exceed the width of the field
+                    if (val & (2**width-1)) != val:
+                        err_msg = "attempted to assign value that does not fit in bit field width {:d}".format(width)
+                        raise ValueError(err_msg)
                     self.packed &= ~((2**width-1) << offset) #clear the field
                     self.packed |= (val & (2**width-1)) << offset #set the field
                 setattr(self, k, property(fget, fset, None, None))
                 init_offset += v.width
+
+class BitFieldUnion(metaclass=BitFieldUnionMeta):
+    """Painless bit packing"""
+    def __init__(self, **kwargs):
+        super(BitFieldUnion, self).__init__()
+        if "packed" in kwargs and len(kwargs) > 1:
+            raise AttributeError("unable to set both `packed` and another bit field")
+        for k,v in kwargs.items():
+            setattr(self, k, v)
