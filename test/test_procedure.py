d3ad90c8dbfdde5ee5c65b1429269ee2a9f090f7
Graham Rowlands <grahamrow@gmail.com>
2016-04-25 10:04:56 -0400

    Updated procedure unit test.

--- test/test_procedure.py
+++ test/test_procedure.py
@@ -1,37 +1,37 @@
 import unittest
 
-from pycontrol.instruments.instrument import Instrument, Command, FloatCommand, IntCommand
+from pycontrol.instruments.instrument import Instrument, StringCommand, FloatCommand, IntCommand
 from pycontrol.procedure import Procedure, FloatParameter, Quantity
 
 class TestInstrument1(Instrument):
-	frequency = FloatCommand("frequency", get_string="frequency?", set_string="frequency {:g}", value_range=(0.1, 10))
-	serial_number = IntCommand("serial number", get_string="serial?")
-	mode = Command("enumerated mode", scpi_string=":mode", allowed_values=["A", "B", "C"])
+	frequency = FloatCommand(get_string="frequency?", set_string="frequency {:g}", value_range=(0.1, 10))
+	serial_number = IntCommand(get_string="serial?")
+	mode = StringCommand(scpi_string=":mode", allowed_values=["A", "B", "C"])
 
 class TestInstrument2(Instrument):
-	frequency = FloatCommand("frequency", get_string="frequency?", set_string="frequency {:g}", value_range=(0.1, 10))
-	serial_number = IntCommand("serial number", get_string="serial?")
-	mode = Command("enumerated mode", scpi_string=":mode", allowed_values=["A", "B", "C"])
+	frequency = FloatCommand(get_string="frequency?", set_string="frequency {:g}", value_range=(0.1, 10))
+	serial_number = IntCommand(get_string="serial?")
+	mode = StringCommand(scpi_string=":mode", allowed_values=["A", "B", "C"])
 
 class TestInstrument3(Instrument):
-	power = FloatCommand("power", get_string="power?")
-	serial_number = IntCommand("serial number", get_string="serial?")
-	mode = Command("enumerated mode", scpi_string=":mode", allowed_values=["A", "B", "C"])
+	power = FloatCommand(get_string="power?")
+	serial_number = IntCommand(get_string="serial?")
+	mode = StringCommand(scpi_string=":mode", allowed_values=["A", "B", "C"])
 
 class TestProcedure(Procedure):
 
     # Create instances of instruments
-    fake_instr_1 = TestInstrument1("Fake Instrument 1", "FAKE::RESOURE::NAME")
-    fake_instr_2 = TestInstrument2("Fake Instrument 2", "FAKE::RESOURE::NAME")
-    fake_instr_3 = TestInstrument3("Fake Instrument 2", "FAKE::RESOURE::NAME")
+    fake_instr_1 = TestInstrument1("FAKE::RESOURE::NAME")
+    fake_instr_2 = TestInstrument2("FAKE::RESOURE::NAME")
+    fake_instr_3 = TestInstrument3("FAKE::RESOURE::NAME")
 
     # Parameters
-    freq_1 = FloatParameter("Frequency 1", unit="Hz")
-    freq_2 = FloatParameter("Frequency 2", unit="Hz")
+    freq_1 = FloatParameter(unit="Hz")
+    freq_2 = FloatParameter(unit="Hz")
 
     # Quantities
-    power = Quantity("Power", unit="Watts")
-    clout = Quantity("Clout", unit="Trumps")
+    power = Quantity(unit="Watts")
+    clout = Quantity(unit="Trumps")
 
     def run(self):
         pass
