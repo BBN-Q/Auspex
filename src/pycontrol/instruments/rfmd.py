c374ceb0c457047df7e5deca8e84e5791ce4792a
CoherenceController <buq-github@bbn.com>
2016-04-26 18:16:30 -0400

    Added basic instrument wrapper for RFMD voltage controller attenuator
    --GER

---
+++ src/pycontrol/instruments/rfmd.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
+
+class Attenuator(object):
+    """Simple wrapper for using the RFMD voltage controller attenuator.
+    Remember that the calibration values for attenuation will be referenced
+    to a certain point in the circuit."""
+    def __init__(self, calibration_file, voltage_supply_method, voltage_control_method):
+        super(Attenuator, self).__init__()
+        self.name = "RFMD VC Attenuator"
+        df = pd.read_csv(calibration_file, sep="\t")
+        attenuator_interp = interp1d(df["Attenuation"], df["Control Voltage"])
+        self.attenuator_lookup = lambda x : float(attenuator_interp(x))
+        self.voltage_control_method = voltage_control_method
+        voltage_supply_method(3.0)
+
+    # Add a property setter only
+    def set_attenuation(self, value):
+        self.voltage_control_method(self.attenuator_lookup(value))
+
+    attenuation = property(None, set_attenuation)
+
+    def __repr__(self):
+        return self.name
