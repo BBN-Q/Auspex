e06ab607b8b159780ffa6dc22f21311feb42f6db
Graham Rowlands <grahamrow@gmail.com>
2016-06-14 10:18:07 -0400

    Averager back in action, largely

--- src/pycontrol/filters/average.py
+++ src/pycontrol/filters/average.py
@@ -35,31 +35,40 @@ class Average(Filter):
     def update_descriptors(self):
         logger.debug("Updating averager descriptors based on input descriptor: %s.", self.data.descriptor)
         descriptor_in = self.data.descriptor
+        names = [a.name for a in descriptor_in.axes]
 
         # Convert named axes to an index
         if isinstance(self.axis, str):
-            names = [a.name for a in descriptor_in.axes]
             if self.axis not in names:
                 raise ValueError("Could not find axis {} within the DataStreamDescriptor {}".format(self.axis, self.descriptor_in))
             self.axis = names.index(self.axis)
+            
+        logger.debug("Averaging over axis #%d: %s", self.axis, names[self.axis])
 
         new_axes = descriptor_in.axes[:]
         self.num_averages = new_axes.pop(self.axis).num_points()
+        logger.debug("Number of partial averages is %d", self.num_averages)
         descriptor_out = DataStreamDescriptor()
         descriptor_out.axes = new_axes
 
-        if self.axis == 0:
-            self.points_before_partial_average = descriptor_in.num_points_through_axis(0)
-        else:
-            self.points_before_partial_average = descriptor_in.num_points_through_axis(self.axis-1)
+
+        self.points_before_partial_average = descriptor_in.num_points_through_axis(self.axis+1)
         self.points_before_final_average   = descriptor_in.num_points_through_axis(self.axis)
 
-        self.data_dims = descriptor_in.data_dims(fortran=True) # Minding that we define axes in fortan ordering
-        self.avg_dims = list(reversed(self.data_dims[0:self.axis])) # Back to C ordering for numpy
-        self.sum_so_far = np.zeros(self.avg_dims)
+        self.data_dims = descriptor_in.data_dims() 
+        self.avg_dims = self.data_dims[self.axis+1:]
+        logger.debug("Data dimensions are %s", self.data_dims)
+        logger.debug("Averaging dimensions are %s", self.avg_dims)
 
-        self.partial_average.set_descriptor(descriptor_in)
-        self.final_average.set_descriptor(descriptor_out)
+        if self.avg_dims == []:
+            logger.debug("Performing scalar average!")
+            self.points_before_final_average = descriptor_in.num_points()
+            self.sum_so_far = 0.0
+        else:
+            self.sum_so_far = np.zeros(self.avg_dims)
+
+        self.partial_average.descriptor = descriptor_in
+        self.final_average.descriptor = descriptor_out
 
         for stream in self.partial_average.output_streams:
             logger.debug("\tnow setting stream %s to %s", stream, descriptor_in)
@@ -75,8 +84,9 @@ class Average(Filter):
 
     async def run(self):
         logger.debug("Running averager async loop")
+        logger.debug("Points before partial average: %s.", self.points_before_partial_average)
         logger.debug("Points before final average: %s.", self.points_before_final_average)
-        import ipdb; ipdb.set_trace()
+        # import ipdb; ipdb.set_trace()
 
         if self.points_before_final_average is None:
             raise Exception("Average has not been initialized. Run 'update_descriptors'")
@@ -87,6 +97,7 @@ class Average(Filter):
         # We only need to accumulate up to the averaging axis
         # BUT we may get something longer at any given time!
         temp = np.empty(self.data.input_streams[0].num_points())
+        logger.debug("Established averager buffer of size %d", self.data.input_streams[0].num_points())
 
         while True:
             if self.data.input_streams[0].done():
@@ -96,7 +107,8 @@ class Average(Filter):
                         break
 
             new_data = await self.data.input_streams[0].queue.get()
-            print("{} got data {}".format(self.name, new_data))
+            logger.debug("%s got data %s", self.name, new_data)
+            logger.debug("Now has %d of %d points.", self.data.input_streams[0].points_taken, self.data.input_streams[0].num_points())
             
             # todo: handle unflattened data separately
             if len(new_data.shape) > 1:
@@ -110,15 +122,17 @@ class Average(Filter):
             if completed_averages + num_partials > self.num_averages:
                 num_partials = self.num_averages - completed_averages
 
+            logger.debug("Just got enough points for %d partial averages.", num_partials)
             for i in range(num_partials):
                 # print("Adding to sum")
                 b = i*self.points_before_partial_average
                 e = b + self.points_before_partial_average
-                self.sum_so_far += temp[b:e]
+                self.sum_so_far += np.reshape(temp[b:e], self.avg_dims)
                 # print("Sum is now {}".format(self.sum_so_far))
 
             completed_averages += num_partials
-            # print("Have completed {} averages".format(completed_averages))
+            logger.debug("Now has %d of %d averages.", completed_averages, self.num_averages)
+            
 
             # Shift any extra data back to the beginnig of the array
             extra = idx - num_partials*self.points_before_partial_average
@@ -126,6 +140,7 @@ class Average(Filter):
             idx = extra
 
             if num_partials > 0:
+                logger.debug("Now has %d of %d averages.", completed_averages, self.num_averages)
                 for output_stream in self.partial_average.output_streams:
                     await output_stream.push(self.sum_so_far/completed_averages)
 
