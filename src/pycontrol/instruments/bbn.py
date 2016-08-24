7db4940b4dcbd40e00a32ecda3c6b17eb377bf92
Graham Rowlands <grahamrow@gmail.com>
2016-08-24 15:30:43 -0400

    Renamed BBN attenuator to DigitalAttenuator for compatibility with PyQLab.

--- src/pycontrol/instruments/bbn.py
+++ src/pycontrol/instruments/bbn.py
@@ -10,13 +10,13 @@ from .instrument import Instrument, VisaInterface
 from types import MethodType
 
 
-class Attenuator(Instrument):
+class DigitalAttenuator(Instrument):
     """BBN 3 Channel Instrument"""
 
     NUM_CHANNELS = 3
 
     def __init__(self, resource_name):
-        super(Attenuator, self).__init__(resource_name, interface_type="VISA")
+        super(DigitalAttenuator, self).__init__(resource_name, interface_type="VISA")
         self.name = "BBN Digital Attenuator"
         self.interface._resource.baud_rate = 115200
         self.interface._resource.read_termination = u"\r\n"
@@ -36,11 +36,11 @@ class Attenuator(Instrument):
         assert chan > 0 and chan <= cls.NUM_CHANNELS, "Invalid channel requested: channel ({:d}) must be between 1 and {:d}".format(chan, cls.NUM_CHANNELS)
 
     def get_attenuation(self, chan):
-        Attenuator.channel_check(chan)
+        DigitalAttenuator.channel_check(chan)
         return float(self.interface.query("GET {:d}".format(chan)))
 
     def set_attenuation(self, chan, val):
-        Attenuator.channel_check(chan)
+        DigitalAttenuator.channel_check(chan)
         self.interface.write("SET {:d} {:.1f}".format(chan, val))
         assert self.interface.read() == "Setting channel {:d} to {:.2f}".format(chan, val)
         assert self.interface.read() == "END"
