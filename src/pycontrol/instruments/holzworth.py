5f1eb0841bcff74867557bada6a68c106619a09c
Graham Rowlands <grahamrow@gmail.com>
2016-08-30 11:32:17 -0400

    Holzworth driver fleshed out :construction: (largely untested). Getters and setters automatically added from properties

--- src/pycontrol/instruments/holzworth.py
+++ src/pycontrol/instruments/holzworth.py
@@ -6,10 +6,21 @@
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 
-from pycontrol.instruments.instrument import Instrument
+from pycontrol.instruments.instrument import Instrument, MetaInstrument
+from pycontrol.log import logger
 import ctypes
 
-class HS9000(Instrument):
+class MakeSettersGetters(MetaInstrument):
+    def __init__(self, name, bases, dct):
+        super(MakeSettersGetters, self).__init__(name, bases, dct)
+
+        for k,v in dct.items():
+            if isinstance(v, property):
+                logger.debug("Adding '%s' command to Holzworth", k)
+                setattr(self, 'set_'+k, v.fset)
+                setattr(self, 'get_'+k, v.fget)
+
+class HS9000(Instrument, metaclass=MakeSettersGetters):
     """Holzworth HS9000 microwave source"""
     instrument_type = "Microwave Source"
     _lib = ctypes.CDLL("HolzworthMulti64.dll")
@@ -27,29 +38,60 @@ class HS9000(Instrument):
     def query(self, scpi_string):
         return self._lib.usbCommWrite(self.resource_name.encode('ascii'), scpi_string.encode('ascii')).decode('ascii')
 
+    def ch_query(self, scpi_string):
+        chan_string = ":CH{}".format(self.chan)
+        scpi_string = chan_string + scpi_string
+        return self._lib.usbCommWrite(self.resource_name.encode('ascii'), scpi_string.encode('ascii')).decode('ascii')
+
     @property
     def frequency(self):
-        v = self.query(":CH{}:FREQ?".format(self.chan))
+        v = self.ch_query(":FREQ?")
         return float(v.split()[0])*1e6
     @frequency.setter
     def frequency(self, value):
-        self.query(":CH{}:FREQ:{} GHz".format(self.chan, value*1e-9))
+        self.ch_query(":FREQ:{} GHz".format(value*1e-9))
+
+    @property
+    def power(self):
+        v = self.ch_query(":PWR?")
+        return float(v.split()[0])
+    @power.setter
+    def power(self, value):
+        self.ch_query(":PWR:{} dBm".format(value))
+
+    @property
+    def phase(self):
+        v = self.ch_query(":PHASE?")
+        return float(v.split()[0])
+    @phase.setter
+    def phase(self, value):
+        self.ch_query(":PHASE:{} deg".format(value))
+
+    @property
+    def output(self):
+        v = self.ch_query(":PWR:RF?")
+        return bool(v.split()[0])
+    @output.setter
+    def output(self, value):
+        if value:
+            self.ch_query(":PWR:RF:ON")
+        else:
+            self.ch_query(":PWR:RF:OFF")
+
+    @property
+    def reference(self):
+        v = self.query(":REF:STATUS?")
+        return float(v.split()[0])*1e6
+    @reference.setter
+    def reference(self, value):
+        ref_opts = ["INT", "10MHz", "100MHz"]
+        if value in ref_opts:
+            if value == "INT":
+                self.query(":REF:INT:100MHz")
+            else:
+                self.query(":REF:EXT:{}".format(value))
+        else:
+            raise ValueError("Reference must be one of {}.".format(ref_opts))
 
     def __del__(self):
-        pass
-
-    def set_all(self, thing):
-        pass
-
-      #     "address": "HS9004A-393-2", 
-      # "alc": false, 
-      # "deviceName": "HolzworthHS9000", 
-      # "frequency": 5.0471484337709995, 
-      # "gateBuffer": 4e-08, 
-      # "gateDelay": 1e-08, 
-      # "gateMinWidth": 1e-07, 
-      # "mod": false, 
-      # "output": true, 
-      # "power": 14.5, 
-      # "pulse": false, 
-      # "pulseSource": "External"
+        self._lib.close_all()
