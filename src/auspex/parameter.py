37c2801800f25fb43f1020ab147191acb5593166
Graham Rowlands <grahamrow@gmail.com>
2016-11-17 21:23:50 -0500

    Adding dict repr to parameters.

--- src/auspex/parameter.py
+++ src/auspex/parameter.py
@@ -11,12 +11,23 @@ from auspex.log import logger
 class Parameter(object):
     """ Encapsulates the information for an experiment parameter"""
 
-    def __init__(self, name=None, unit=None, default=None):
+    def __init__(self, name=None, unit=None, default=None,
+                 value_range=None, allowed_values=None,
+                 increment=None, snap=None):
         self.name     = name
         self._value   = default
         self.unit     = unit
         self.default  = default
         self.method   = None
+
+        # These are primarily intended for Quince interoperation,
+        # but will maybe be useful for Auspex too...
+        self.value_range    = value_range
+        self.allowed_values = allowed_values
+        self.increment      = increment
+        if snap:
+            self.increment = snap
+        self.snap           = snap
 
         # Hooks to be called before or after updating a sweep parameter
         self.pre_push_hooks = []
@@ -50,6 +61,44 @@ class Parameter(object):
             result += ",unit='%s'" % self.unit
         return result + ")>"
 
+    def dict_repr(self):
+        """Return a dictionary representation. Intended for Quince interop."""
+        param_dict = {}
+        param_dict['name'] = self.name
+
+        if isinstance(self, FloatParameter):
+            param_dict['type'] = 'float' 
+        elif isinstance(self, IntParameter):
+            param_dict['type'] = 'int' 
+        elif isinstance(self, BoolParameter):
+            param_dict['type'] = 'bool' 
+        elif isinstance(self, FilenameParameter):
+            param_dict['type'] = 'filename' 
+        elif isinstance(self, Parameter):
+            if self.allowed_values:
+                param_dict['type'] = 'combo'
+                param_dict['choices'] = self.allowed_values
+            else:
+                param_dict['type'] = 'str' 
+
+        if isinstance(self, FloatParameter) or isinstance(self, IntParameter):
+            if self.value_range:
+                param_dict['low'] = min(self.value_range)
+                param_dict['high'] = max(self.value_range)
+            else:
+                param_dict['low'] = -1e15
+                param_dict['high'] = 1e15
+                param_dict['increment'] = 2e14
+
+            if self.increment:
+                param_dict['increment'] = self.increment
+            else:
+                param_dict['increment'] = 0.05*(param_dict['high']-param_dict['low'])
+            
+            param_dict['snap'] = self.snap
+        
+        return param_dict
+
     def assign_method(self, method):
         logger.debug("Setting method of Parameter %s to %s" % (self.name, str(method)) )
         self.method = method
@@ -64,6 +113,9 @@ class Parameter(object):
             # logger.debug("Calling post_push_hooks of Parameter %s with value %s" % (self.name, self._value) )
             for pph in self.post_push_hooks:
                 pph()
+
+class FilenameParameter(Parameter):
+    pass
 
 class ParameterGroup(Parameter):
     """ An array of Parameters """
@@ -142,3 +194,21 @@ class IntParameter(Parameter):
     def __repr__(self):
         result = super(IntParameter, self).__repr__()
         return result.replace("<Parameter", "<IntParameter", 1)
+
+class BoolParameter(Parameter):
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        try:
+            self._value = bool(value)
+        except ValueError:
+            raise ValueError("BoolParameter given non-bool value of "
+                             "type '%s'" % type(value))
+
+    def __repr__(self):
+        result = super(BoolParameter, self).__repr__()
+        return result.replace("<Parameter", "<BoolParameter", 1)
