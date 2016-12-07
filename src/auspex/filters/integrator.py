b0a96dc2bdf12cd43f855a663998cb21911d4410
Blake Johnson <bjohnson@bbn.com>
2016-12-07 12:15:00 -0500

    Refactor Integrator to construct simple kernels from parameters.
    --with CAR

--- src/auspex/filters/integrator.py
+++ src/auspex/filters/integrator.py
@@ -26,9 +26,14 @@ class KernelIntegrator(Filter):
     frequency = FloatParameter()
 
     """Integrate with a given kernel. Kernel will be padded/truncated to match record length"""
-    def __init__(self, kernel=None, **kwargs):
+    def __init__(self, **kwargs):
         super(KernelIntegrator, self).__init__(**kwargs)
-        self.kernel.value = kernel
+        self.kernel.value = kwargs['kernel']
+        self.bias.value = kwargs['bias']
+        self.simple_kernel.value = kwargs['simple_kernel']
+        self.box_car_start.value = kwargs['box_car_start']
+        self.box_car_stop.value = kwargs['box_car_stop']
+        self.frequency.value = kwargs['frequency']
 
     def update_descriptors(self):
         if self.kernel.value is None:
@@ -36,12 +41,23 @@ class KernelIntegrator(Filter):
 
         logger.debug('Updating KernelIntegrator "%s" descriptors based on input descriptor: %s.', self.name, self.sink.descriptor)
 
-        # pad or truncate the kernel to match the record length
         record_length = self.sink.descriptor.axes[-1].num_points()
-        if self.kernel.value.size < record_length:
-            self.aligned_kernel = np.append(self.kernel.value, np.zeros(record_length-self.kernel.value.size, dtype=np.complex128))
+        if self.simple_kernel.value:
+            time_pts = self.sink.descriptor.axes[-1].points
+            time_step = time_pts[1] - time_pts[0]
+            kernel = np.zeros(record_length, dtype=np.complex128)
+            sample_start = int(self.box_car_start.value / time_step)
+            sample_stop = int(self.box_car_stop.value / time_step) + 1
+            kernel[sample_start:sample_stop] = 1.0
+            # add modulation
+            kernel *= np.exp(2j * np.pi * self.frequency.value * time_step * time_pts)
         else:
-            self.aligned_kernel = self.kernel.value.resize(record_length)
+            kernel = eval(self.kernel.value.encode('unicode_escape'))
+        # pad or truncate the kernel to match the record length
+        if kernel.size < record_length:
+            self.aligned_kernel = np.append(kernel, np.zeros(record_length-kernel.size, dtype=np.complex128))
+        else:
+            self.aligned_kernel = np.resize(kernel, record_length)
 
         # Integrator reduces and removes axis on output stream
         # update output descriptors
