a9a6ea83da2c7da0eb2d6fa5c446720421c69a74
Rob McGurrin <rmcgurrin@bbn.com>
2016-12-22 10:54:17 -0500

    Add optimize_amplitude
    Port the functionality from matlab.  This function will have to be
    refactored once we figure out how to integrate the calibration
    loop into auspex.  For now, the "loop" is simulated using the
    update_data function pointer passed into the routine.  This simulates
    receving a sequence from the digitizer with an updated pulse
    amplitude for testing.

--- test/test_pulsecal.py
+++ test/test_pulsecal.py
@@ -10,6 +10,7 @@ import unittest
 import numpy as np
 
 import auspex.pulsecal.phase_estimation as pe
+import auspex.pulsecal.optimize_amplitude as oa
 
 def simulate_measurement(amp, target, numPulses):
     
@@ -52,6 +53,30 @@ class PhaseEstimateTestCase(unittest.TestCase):
         phase, sigma = pe.phase_estimation(data, vardata, verbose=True)
         self.assertAlmostEqual(phase,-1.2012,places=4)
         self.assertAlmostEqual(sigma,0.0245,places=4)
+        
+class OptimizeAmplitudeTestCase(unittest.TestCase):
+
+    def test_simulated_measurement(self):
+        
+        numPulses = 9
+        amp = .55
+        direction = 'X'
+        target = np.pi
+        
+        # NOTE: this function is a place holder to simulate an AWG generating
+        # a sequence and a digitizer receiving the sequence.  This function
+        # is passed into the optimize_amplitude routine to be able to update
+        # the amplitude as part of the optimization loop.
+        def update_data(amp):
+            data, vardata =  simulate_measurement(amp, target, numPulses)   
+            return data,vardata
+        
+        # Verify output matches what was previously seen by matlab
+        amp_out = oa.optimize_amplitude(amp, direction, target, update_data)
+        
+        # NOTE: expected result is from the same input fed to the matlab
+        # routine
+        self.assertAlmostEqual(phase,0.3400,places=4)
 
 
 if __name__ == '__main__':
