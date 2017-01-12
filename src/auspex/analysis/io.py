74d612b901cf3640af50e2ee728e59df1e8599be
Graham Rowlands <grahamrow@gmail.com>
2017-01-12 16:40:48 -0500

    Add group name in io loader

--- src/auspex/analysis/io.py
+++ src/auspex/analysis/io.py
@@ -9,13 +9,13 @@ from auspex.stream import DataStream, DataAxis, SweepAxis, DataStreamDescriptor,
 import h5py
 import numpy as np
 
-def load_from_HDF5(filename):
+def load_from_HDF5(filename, groupname='main'):
     with h5py.File(filename, 'r') as f:
     	# Reconstruct the descriptor
     	descriptor = DataStreamDescriptor()
-    	axis_refs = f['descriptor']
+    	axis_refs = f['groupname']['descriptor']
     	for ref in reversed(axis_refs):
-    		ax = f[ref]
+    		ax = f['groupname'][ref]
     		if len(ax.dtype) > 1:
     			# Unstructured
 
@@ -31,7 +31,7 @@ def load_from_HDF5(filename):
     			points = ax[:]
     			descriptor.add_axis(DataAxis(name, points=points, unit=unit))
 
-    	data = f['data'][:]
+    	data = f['groupname']['data'][:]
     	return data, descriptor
 
 if __name__ == '__main__':
