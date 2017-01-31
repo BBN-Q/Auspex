BBN-Q/Auspex

commit 740e70c75634fd9c58a902f89249564d97eace78

Fix premature refinement. Closes #51.

src/auspex/sweep.py
@@ -38,6 +38,18 @@ class Sweeper(object):
             for j in range(i,-1,-1):
                 await self.axes[j].update()
 
+    async def check_for_refinement(self):
+        imax = len(self.axes)-1
+        if imax < 0:
+            return True
+        else:
+            i=0
+            while i<imax and self.axes[i].step==0:
+                i += 1
+            # Need to update parameters from outer --> inner axis
+            for j in range(i,-1,-1):
+                await self.axes[j].check_for_refinement()
+
     def done(self):
         return np.all([a.done for a in self.axes])
 

src/auspex/experiment.py
@@ -317,6 +317,9 @@ class Experiment(metaclass=MetaExperiment):
             logger.debug("Starting a new run.")
             await self.run()
 
+            # See if the axes want to extend themselves
+            await self.sweeper.check_for_refinement()
+
             # Update progress bars
             if self.progressbar is not None:
                 self.progressbar.update()

src/auspex/stream.py
@@ -128,6 +128,7 @@ class SweepAxis(DataAxis):
             self.step += 1
             self.done = False
 
+    async def check_for_refinement(self):
         if not self.done and self.step==self.num_points():
             # Check to see if we need to perform any refinements
             await asyncio.sleep(0.1)

test/test_adapt_1d.py
@@ -1,0 +1,100 @@
+# Copyright 2016 Raytheon BBN Technologies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+import unittest
+import asyncio
+import os
+import numpy as np
+import h5py
+from adapt.refine import refine_1D
+
+from auspex.experiment import Experiment
+from auspex.parameter import FloatParameter
+from auspex.stream import DataStream, DataAxis, DataStreamDescriptor, OutputConnector
+from auspex.filters.debug import Print
+from auspex.filters.io import WriteToHDF5
+from auspex.log import logger
+from auspex.analysis.io import load_from_HDF5
+
+import logging
+logger.setLevel(logging.DEBUG)
+
+
+
+class SweptTestExperiment(Experiment):
+    """Here the run loop merely spews data until it fills up the stream. """
+
+    # Parameters
+    temperature = FloatParameter(unit="K")
+
+    # DataStreams
+    resistance = OutputConnector()
+
+    # Constants
+    samples = 5
+    time_val = 0
+
+    def __repr__(self):
+        return "<SweptTestExperiment>"
+
+    async def run(self):
+        logger.debug("Data taker running (inner loop)")
+        await asyncio.sleep(0.002)
+
+        def ideal_tc(t, tc=9.0, k=20.0):
+            return t*1.0/(1.0 + np.exp(-k*(t-tc)))
+
+        await self.resistance.push(ideal_tc(self.temperature.value))
+        # logger.debug("Stream pushed points {}.".format(data_row))
+        # logger.debug("Stream has filled {} of {} points".format(self.resistance.points_taken, self.resistance.num_points() ))
+
+class Adapt1DTestCase(unittest.TestCase):
+
+    def test_writehdf5_1D_adaptive_sweep(self):
+        exp = SweptTestExperiment()
+        if os.path.exists("test_writehdf5_1D_adaptive-0000.h5"):
+            os.remove("test_writehdf5_1D_adaptive-0000.h5")
+        wr = WriteToHDF5("test_writehdf5_1D_adaptive.h5")
+
+        edges = [(exp.resistance, wr.sink)]
+        exp.set_graph(edges)
+
+        async def rf(sweep_axis, exp):
+            # logger.debug("Waiting for writer to catch up...")
+            # await asyncio.sleep(0.5)
+            # return False
+            logger.debug("Running refinement function.")
+            temps = wr.data['temperature']
+            ress  = wr.data['resistance']
+            logger.debug("Temps: {}".format(temps))
+            logger.debug("Ress: {}".format(ress))
+
+            new_temps = refine_1D(temps, ress, all_points=False, criterion="difference", threshold = "one_sigma")
+
+            logger.debug("New temperature values: {}".format(new_temps))
+            if new_temps.size + temps.size > 15:
+                return False
+
+            sweep_axis.add_points(new_temps)
+            logger.debug("Axis points are now: {}".format(sweep_axis.points))
+            return True
+
+        exp.add_sweep(exp.temperature, np.linspace(0,20,5), refine_func=rf)
+        exp.run_sweeps()
+        
+        self.assertTrue(os.path.exists("test_writehdf5_1D_adaptive-0000.h5"))
+        
+        expected_data = np.array([ 0., 5.,10.,15.,20., 7.5, 8.75, 9.375, 9.0625, 8.90625, 8.984375,12.5,17.5, 9.0234375, 8.945312])
+        data, desc = load_from_HDF5(wr.filename.value)
+        actual_data = data['main']['temperature']
+        self.assertTrue(np.sum(np.abs(expected_data - actual_data)) < 0.05)
+
+        os.remove("test_writehdf5_1D_adaptive-0000.h5")
+
+if __name__ == '__main__':
+    unittest.main()

commit bcf52d0bf9237807f6ea85d8cf5c1794083a254d

Fixed doneness :bug: introduced in 740e70c7

src/auspex/sweep.py
@@ -39,16 +39,8 @@ class Sweeper(object):
                 await self.axes[j].update()
 
     async def check_for_refinement(self):
-        imax = len(self.axes)-1
-        if imax < 0:
-            return True
-        else:
-            i=0
-            while i<imax and self.axes[i].step==0:
-                i += 1
-            # Need to update parameters from outer --> inner axis
-            for j in range(i,-1,-1):
-                await self.axes[j].check_for_refinement()
+        for a in self.axes:
+            await a.check_for_refinement()
 
     def done(self):
         return np.all([a.done for a in self.axes])