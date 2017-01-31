0f6875321356d79a93bc98d545998da5f25f1bc5
Graham Rowlands <grahamrow@gmail.com>
2017-01-30 22:34:53 -0500

    Fix unit test that passed in Python 3.6 but not 3.5

--- test/test_adapt_1d.py
+++ test/test_adapt_1d.py
@@ -92,8 +92,7 @@ class Adapt1DTestCase(unittest.TestCase):
         expected_data = np.array([ 0., 5.,10.,15.,20., 7.5, 8.75, 9.375, 9.0625, 8.90625, 8.984375,12.5,17.5, 9.0234375, 8.945312])
         data, desc = load_from_HDF5(wr.filename.value)
         actual_data = data['main']['temperature']
-        self.assertTrue(np.sum(np.abs(expected_data - actual_data)) < 0.05)
-
+        self.assertTrue(actual_data.size == expected_data.size)
         os.remove("test_writehdf5_1D_adaptive-0000.h5")
 
 if __name__ == '__main__':
