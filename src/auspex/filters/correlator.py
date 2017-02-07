d666109f98d84000567639ae9413d01a2ab4c593
Colm Ryan <colm@colmryan.org>
2017-02-07 14:49:12 -0500

    :bug: overzealous import removal in 3d1211a

--- src/auspex/filters/correlator.py
+++ src/auspex/filters/correlator.py
@@ -6,6 +6,8 @@
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 
+import numpy as np
+
 from auspex.stream import InputConnector, OutputConnector
 from auspex.log import logger
 from .elementwise import ElementwiseFilter
