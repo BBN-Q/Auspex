b58c5af3e6b477c6d27e60ff36d68f748e31c8db
CoherenceController <buq-github@bbn.com>
2017-04-02 16:28:11 -0400

    Fix up locking RvsH experiment.
    --GER

--- experiments/Field_Switching_Lockin.py
+++ experiments/Field_Switching_Lockin.py
@@ -26,38 +26,28 @@ class FieldSwitchingLockinExperiment(Experiment):
     # measure_current = FloatParameter(default=3e-6, unit="A")
     resistance      = OutputConnector(unit="Ohm")
 
-    res_reference = 1e6
-    vout = 10e-3
+    res_reference = 1e3
+    vout  = 10e-3
     mag   = AMI430("192.168.5.109")
     # keith = Keithley2400("GPIB0::25::INSTR")
     lock  = SR865("USB0::0xB506::0x2000::002638::INSTR")
 
     def init_instruments(self):
-        # self.keith.triad()
-        # self.keith.conf_meas_res(NPLC=10, res_range=1e5)
-        # self.keith.conf_src_curr(comp_voltage=0.5, curr_range=1.0e-5)
-        # self.keith.current = self.measure_current.value
-
         # Initialize lockin
-        # self.lock.this = that
         self.lock.amp = self.vout
         self.mag.ramp()
+        self.tc_delay = self.lock.measure_delay()
 
-        # self.measure_current.assign_method(self.keith.set_current)
         self.field.assign_method(self.mag.set_field)
-        self.field.add_post_push_hook(lambda: time.sleep(10)) # Field set delay
 
     async def run(self):
         """This is run for each step in a sweep."""
-        # await self.resistance.push(self.keith.resistance)
-        # vout = self.lock.x
-        await self.resistance.push(self.lock.mag*self.res_reference/self.lock.amp)
-        logger.debug("Stream has filled {} of {} points".format(self.resistance.points_taken,
-                                                                self.resistance.num_points() ))
-        await asyncio.sleep(10) # Give the filters some time to catch up?
+
+        await asyncio.sleep(self.tc_delay)
+        await self.resistance.push(self.res_reference/((self.vout/self.lock.mag)-1.0))
+        await asyncio.sleep(0.02) # Give the filters some time to catch up?
 
     def shutdown_instruments(self):
-        # self.keith.current = 0.0e-5
         self.mag.zero()
 
 if __name__ == '__main__':
