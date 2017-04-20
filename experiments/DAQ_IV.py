ffa1a0a4c1fbd6f0e42cbc45b1b3518a9668e52a
CoherenceController <buq-github@bbn.com>
2017-04-20 18:23:16 -0400

    DAQ IV tweaks
    --GER

--- experiments/DAQ_IV.py
+++ experiments/DAQ_IV.py
@@ -10,6 +10,12 @@ import asyncio
 import numpy as np
 import time
 
+# Experiment setup
+# AWG -> 1 kOhm -> Sample
+#     |         |
+#    AI1       AI0
+# AWG Sync -> PFI0
+
 class IVExperiment(Experiment):
 
     awg = Agilent33220A("192.168.5.198")
@@ -17,20 +23,20 @@ class IVExperiment(Experiment):
     amplitude = FloatParameter(default=0.1, unit="V")
     frequency  = 167.0 # FloatParameter(default=167.0, unit="Hz")
 
-    sample_rate = 1e4
+    sample_rate = 5e5
     num_bursts  = 10
 
-    preamp_gain = 50.0
+    preamp_gain = 1
     r_ref       = 1e3
 
-    voltage_input  = OutputConnector(unit="V")
+    current_input  = OutputConnector(unit="V")
     voltage_sample = OutputConnector(unit="V")
 
     def init_streams(self):
         descrip = DataStreamDescriptor()
-        descrip.data_name='voltage_input'
+        descrip.data_name='current_input'
         descrip.add_axis(DataAxis("time", np.arange(int(self.sample_rate*self.num_bursts/self.frequency))/self.sample_rate))
-        self.voltage_input.set_descriptor(descrip)
+        self.current_input.set_descriptor(descrip)
 
         descrip = DataStreamDescriptor()
         descrip.data_name='voltage_sample'
@@ -39,7 +45,12 @@ class IVExperiment(Experiment):
 
     def init_instruments(self):
         # Configure the AWG
+        self.awg.output         = False
+        self.awg.function       = 'Sine'
+        self.awg.load_resistance = self.r_ref
+        self.awg.auto_range     = True
         self.awg.amplitude      = self.amplitude.value # Preset to avoid danger
+        self.awg.dc_offset      = 0.0
         self.awg.frequency      = self.frequency
         self.awg.burst_state    = True
         self.awg.burst_cycles   = self.num_bursts + 2
@@ -50,7 +61,7 @@ class IVExperiment(Experiment):
         # self.frequency.assign_method(self.awg.set_frequency)
 
         # Setup the NIDAQ
-        max_voltage = self.amplitude.value*2.0
+        max_voltage = 10.0 #self.amplitude.value*2.0
         self.num_samples_total = int(self.sample_rate*(self.num_bursts+2)/self.frequency)
         self.num_samples_trimmed = int(self.sample_rate*(self.num_bursts)/self.frequency)
         self.trim_len = int(self.sample_rate/self.frequency)
@@ -68,7 +79,8 @@ class IVExperiment(Experiment):
         # self.analog_input.SetStartTrigRetriggerable(1)
 
     def shutdown_instruments(self):
-        self.awg.output = False
+        self.awg.output     = False
+        self.awg.auto_range = True
         try:
             self.analog_input.StopTask()
             self.analog_input.ClearTask()
@@ -83,12 +95,6 @@ class IVExperiment(Experiment):
         buf = np.empty(2*self.num_samples_total)
         self.analog_input.ReadAnalogF64(self.num_samples_total, -1, DAQmx_Val_GroupByChannel,
                                         buf, 2*self.num_samples_total, byref(self.read), None)
-        await self.voltage_input.push(buf[self.num_samples_total+self.trim_len:self.num_samples_total+self.trim_len+self.num_samples_trimmed])
-        await self.voltage_sample.push(buf[self.trim_len:self.trim_len+self.num_samples_trimmed])
-
-        logger.warning("Stream has filled {} of {} points".format(self.voltage_sample.points_taken,
-                                                                self.voltage_sample.num_points() ))
-        logger.warning("Stream has filled {} of {} points".format(self.voltage_input.points_taken,
-                                                                self.voltage_input.num_points() ))
-        # Seemingly we need to give the filters some time to catch up here...
+        await self.current_input.push(buf[self.num_samples_total+self.trim_len:self.num_samples_total+self.trim_len+self.num_samples_trimmed]/self.r_ref)
+        await self.voltage_sample.push(buf[self.trim_len:self.trim_len+self.num_samples_trimmed]/self.preamp_gain)
         await asyncio.sleep(0.02)
