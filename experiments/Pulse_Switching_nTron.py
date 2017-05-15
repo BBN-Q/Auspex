1dcb884c4ed6f4a098a6fd0b1c142cafa3709031
CoherenceController <buq-github@bbn.com>
2017-05-15 17:41:35 -0400

    Updates to nTron switching code
    --GER

--- experiments/Pulse_Switching_nTron.py
+++ experiments/Pulse_Switching_nTron.py
@@ -6,6 +6,9 @@
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 
+# 0.1 Hz 6dB slope HPF
+# 300 kHz 6dB slope LPF
+
 from auspex.instruments import KeysightM8190A, Scenario, Sequence
 from auspex.instruments.alazar import AlazarATS9870, AlazarChannel
 from auspex.instruments import Agilent33220A
@@ -15,18 +18,9 @@ from auspex.stream import DataStream, DataAxis, DataStreamDescriptor, OutputConn
 from auspex.filters import Print, WriteToHDF5, Averager, Plotter, Channelizer, KernelIntegrator
 from auspex.log import logger
 
-import itertools
 import numpy as np
 import asyncio
 import time, sys, datetime
-
-# import h5py
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# from scipy.interpolate import interp1d
-
-# import auspex.analysis.switching as sw
-from adapt import refine
 
 def switching_pulse(amplitude, duration, delay=25e-9, holdoff=800e-9, total_duration=1200e-09, sample_rate=12e9):
     total_points = int(total_duration*sample_rate)
@@ -56,29 +50,19 @@ def measure_pulse(amplitude, duration, frequency, holdoff=800e-9, total_duration
 class nTronSwitchingExperiment(Experiment):
 
     # Parameters
-    channel_bias         = FloatParameter(default=0.2,  unit="V") # On the 33220A
+    channel_bias         = FloatParameter(default=0.05,  unit="V") # On the 33220A
     gate_bias            = FloatParameter(default=0.0,  unit="V") # On the M8190A
-    gate_pulse_amplitude = FloatParameter(default=0.2,  unit="V") # On the M8190A
-    gate_pulse_duration  = FloatParameter(default=250e-9, unit="s") # On the M8190A
-
-    gate_pulse_durations   = [0]
-    gate_pulse_amplitudes  = [0]
-    gate_biases            = [0]
 
     # Constants (set with attribute access if you want to change these!)
-    attempts           = 256 #1 << 8
+    attempts           = 1 << 8
     samples            = 384 #1024 + 16*20
-    measure_amplitude  = 0.2
+    measure_amplitude  = 0.1
     measure_duration   = 250.0e-9
     measure_frequency  = 100e6
 
     # arb
     sample_rate = 12e9
     repeat_time = 4*2.4e-6 # Picked very carefully for 100ns alignment
-
-    # Sweep axes
-    gate_amps = np.linspace(0.14, 0.4, 124) #np.linspace(0.14, 0.4, 40)#
-    gate_durs = np.arange(160e-12, 10e-9, 80e-12) #np.linspace(160e-12, 10e-9, 40)#
 
     # Things coming back
     voltage     = OutputConnector()
@@ -87,6 +71,11 @@ class nTronSwitchingExperiment(Experiment):
     arb = KeysightM8190A("192.168.5.108")
     awg = Agilent33220A("192.168.5.198")
     alz = AlazarATS9870("1")
+
+    def __init__(self, gate_amps, gate_durs):
+        self.gate_amps = gate_amps
+        self.gate_durs = gate_durs
+        super(nTronSwitchingExperiment, self).__init__()
 
     def init_instruments(self):
 
@@ -120,7 +109,7 @@ class nTronSwitchingExperiment(Experiment):
             'trigger_source': 'Ext',
             'vertical_coupling': 'AC',
             'vertical_offset': 0.0,
-            'vertical_scale': 0.4,
+            'vertical_scale': 0.1,
         }
         self.alz.set_all(alz_cfg)
         self.loop.add_reader(self.alz.get_socket(self.ch), self.alz.receive_data, self.ch, self.voltage)
@@ -143,9 +132,9 @@ class nTronSwitchingExperiment(Experiment):
         self.arb.set_marker_level_low(0.0, channel=2, marker_type="sync")
         self.arb.set_marker_level_high(1.5, channel=2, marker_type="sync")
 
-        self.setup_arb(self.gate_bias.value, self.gate_pulse_amplitude.value, self.gate_pulse_duration.value) # Sequencing goes here
+        self.setup_arb() #self.gate_bias.value, self.gate_pulse_amplitude.value, self.gate_pulse_duration.value) # Sequencing goes here
 
-    def setup_arb(self, gate_bias, gate_pulse_amplitude, gate_pulse_duration):
+    def setup_arb(self): #, gate_bias, gate_pulse_amplitude, gate_pulse_duration):
         self.arb.abort()
         self.arb.delete_all_waveforms()
         self.arb.reset_sequence_table()
@@ -160,17 +149,9 @@ class nTronSwitchingExperiment(Experiment):
         self.arb.upload_waveform(wf_data, seg_id, channel=1)
         seg_ids_ch1.append(seg_id)
 
-        for amp in self.gate_amps:
-            for dur in self.gate_durs:
-                # For the switching pulses along the gate
-                wf      = switching_pulse(amplitude=amp, duration=dur)#self.gate_pulse_duration.value)
-                wf_data = KeysightM8190A.create_binary_wf_data(wf)
-                seg_id  = self.arb.define_waveform(len(wf_data), channel=2)
-                self.arb.upload_waveform(wf_data, seg_id, channel=2)
-                seg_ids_ch2.append(seg_id)
-
         # Build in a delay between sequences
         settle_pts = int(640*np.ceil(self.repeat_time * self.sample_rate / 640))
+        # settle_pts2 = int(640*np.ceil(8*2.4e-9 * self.sample_rate / 640))
 
         scenario = Scenario()
         seq = Sequence(sequence_loop_ct=self.attempts*len(self.gate_amps)*len(self.gate_durs))
@@ -179,6 +160,15 @@ class nTronSwitchingExperiment(Experiment):
             seq.add_idle(settle_pts, 0.0)
         scenario.sequences.append(seq)
         self.arb.upload_scenario(scenario, start_idx=0, channel=1)
+
+        for amp in self.gate_amps:
+            for dur in self.gate_durs:
+                # For the switching pulses along the gate
+                wf      = switching_pulse(amplitude=amp, duration=dur) #self.gate_pulse_duration.value)
+                wf_data = KeysightM8190A.create_binary_wf_data(wf)
+                seg_id  = self.arb.define_waveform(len(wf_data), channel=2)
+                self.arb.upload_waveform(wf_data, seg_id, channel=2)
+                seg_ids_ch2.append(seg_id)
 
         scenario = Scenario()
         seq = Sequence(sequence_loop_ct=self.attempts)
@@ -202,7 +192,8 @@ class nTronSwitchingExperiment(Experiment):
         # Baked in data axes
         descrip = DataStreamDescriptor()
         descrip.add_axis(DataAxis("time", 1e-9*np.arange(self.samples)))
-        descrip.add_axis(DataAxis("gate_pulse_duration", self.gate_durs))
+        if len(self.gate_durs) > 1:
+            descrip.add_axis(DataAxis("gate_pulse_duration", self.gate_durs))
         descrip.add_axis(DataAxis("gate_pulse_amplitude", self.gate_amps))
         descrip.add_axis(DataAxis("attempt", range(self.attempts)))
 
@@ -237,21 +228,14 @@ class nTronSwitchingExperiment(Experiment):
 
 if __name__ == '__main__':
     exp = nTronSwitchingExperiment()
-    plot = Plotter(name="Demod!", plot_mode="real", plot_dims=1)
-    plot_ki = Plotter(name="Ki!", plot_mode="real", plot_dims=1)
+    plot = Plotter(name="Demod!", plot_mode="real", plot_dims=2)
+    plot_ki = Plotter(name="Ki!", plot_mode="real", plot_dims=2)
     plot_avg = Plotter(name="Avg!", plot_mode="real")
     plot_raw1 = Plotter(name="Raw!", plot_mode="real", plot_dims=1)
     plot_raw2 = Plotter(name="Raw!", plot_mode="real", plot_dims=2)
     demod = Channelizer(frequency=exp.measure_frequency, decimation_factor=4, bandwidth=20e6)
-    kernel_params = {}
-    kernel_params['kernel'] = 0
-    kernel_params['bias'] = 0
-    kernel_params['simple_kernel'] = True
-    kernel_params['box_car_start'] = 1e-7
-    kernel_params['box_car_stop']  = 3.8e-7
-    kernel_params['frequency'] = 0.0
 
-    ki = KernelIntegrator(**kernel_params)
+    ki = KernelIntegrator(kernel=0, bias=0, simple_kernel=True, box_car_start=1e-7, box_car_stop=3.8e-7, frequency=0.0)
     avg = Averager(axis="attempt")
 
     samp      = "c1r4"
@@ -263,14 +247,14 @@ if __name__ == '__main__':
 
     edges = [(exp.voltage, demod.sink),
             # (exp.voltage, wr_raw.sink),
-            # (exp.voltage, plot_raw1.sink),
+            (exp.voltage, plot_raw1.sink),
             # (exp.voltage, plot_raw2.sink),
             (demod.source, ki.sink),
             # (ki.source, plot_ki.sink),
-            # (ki.source, avg.sink),
+            (ki.source, avg.sink),
             (ki.source, wr_int.sink),
-            # (avg.final_average, plot_avg.sink),
-            # (demod.source, plot.sink),
+            (avg.final_average, plot_avg.sink),
+            (demod.source, plot.sink),
             ]
     exp.set_graph(edges)
 
