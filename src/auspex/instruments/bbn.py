7de6137908fcc1bcc565e71564b7d93cee5afecf
Diego Ristè <diego.riste@raytheon.com>
2018-01-09 18:30:26 -0500

    Comment for TDM

--- src/auspex/instruments/bbn.py
+++ src/auspex/instruments/bbn.py
@@ -498,7 +498,7 @@ class APS2(Instrument, metaclass=MakeSettersGetters):
     def set_all(self, settings_dict, prefix=""):
         # Pop the channel settings
         settings = deepcopy(settings_dict)
-        if 'tx_channels' in settings:
+        if 'tx_channels' in settings: # identify a TDM
             quad_channels = settings.pop('tx_channels')
         # Call the non-channel commands
         super(APS2, self).set_all(settings)
