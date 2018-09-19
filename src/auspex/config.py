1a9540d1459925e711eddde0e0a45e13cbddf748
Graham Rowlands <grahamrow@gmail.com>
2018-09-19 11:43:47 -0400

    Pare down config to drop YAML infrastructure

--- src/auspex/config.py
+++ src/auspex/config.py
@@ -30,13 +30,12 @@ auspex_dummy_mode = False
 # plotter before starting a new one.
 single_plotter_mode = False
 
-# This holds a reference to the most
-# recent plotters.
-last_plotter_process = None
-last_extra_plotter_process = None
+# Set generator from qubit sidebanding
+# This sets the generator frequency based on 
+# the requested qubit frequency and sidebanding.
+qubit_IF_priority = False
 
-# Config directory
-meas_file         = None
+# Configuration information
 AWGDir            = None
 ConfigurationFile = None
 KernelDir         = None
@@ -49,119 +48,3 @@ def load_db():
     global db_file
     if os.getenv('BBN_DB'):
         db_file = os.getenv("BBN_DB")
-
-def find_meas_file():
-    global meas_file
-    # First default to any manually set options in the globals
-    if meas_file:
-        return os.path.abspath(meas_file)
-    # Next use the meas file location in the environment variables
-    if os.getenv('BBN_MEAS_FILE'):
-        return os.getenv('BBN_MEAS_FILE')
-    raise Exception("Could not find the measurement file in the environment variables or the auspex globals.")
-
-class Include():
-    def __init__(self, filename):
-        self.filename = filename
-        with open(filename, 'r') as f:
-            self.data = yaml.load(f, Loader=yaml.RoundTripLoader)
-    def __getitem__(self, key):
-        return self.data[key]
-    def __setitem__(self, key, value):
-        self.data[key] = value
-    def items(self):
-        return self.data.items()
-    def keys(self):
-        return self.data.keys()
-    def write(self):
-        with open(self.filename+".tmp", 'w') as fid:
-            yaml.dump(self.data, fid, Dumper=yaml.RoundTripDumper)
-        move(self.filename+".tmp", self.filename)
-    def pop(self, key):
-        return self.data.pop(key)
-
-class Loader(yaml.RoundTripLoader):
-    def __init__(self, stream):
-        try:
-            self._root = os.path.split(stream.name)[0]
-        except AttributeError:
-            self._root = os.path.curdir
-        super().__init__(stream)
-
-    def include(self, node):
-        shortname = self.construct_scalar(node)
-        filename = os.path.abspath(os.path.join(
-            self._root, shortname
-        ))
-        return Include(filename)
-
-class Dumper(yaml.RoundTripDumper):
-    def include(self, data):
-        data.write()
-        return self.represent_scalar(u'!include', data.filename)
-
-class FlatDumper(yaml.RoundTripDumper):
-    def include(self, data):
-        return self.represent_mapping('tag:yaml.org,2002:map', data.data)
-
-def load_meas_file(filename=None):
-    global LogDir, KernelDir, AWGDir, meas_file
-
-    if filename:
-        meas_file = filename
-    else:
-        meas_file = find_meas_file()
-
-    with open(meas_file, 'r') as fid:
-        Loader.add_constructor('!include', Loader.include)
-        load = Loader(fid)
-        code = load.get_single_data()
-        load.dispose()
-
-    # Get the config values out of the measure_file.
-    if not 'config' in code.keys():
-        raise KeyError("Could not find config section of the yaml file.")
-
-    if 'AWGDir' in code['config'].keys():
-        AWGDir = os.path.abspath(code['config']['AWGDir'])
-    else:
-        raise KeyError("Could not find AWGDir in the YAML config section")
-
-    if 'KernelDir' in code['config'].keys():
-        KernelDir = os.path.abspath(code['config']['KernelDir'])
-    else:
-        raise KeyError("Could not find KernelDir in the YAML config section")
-
-    if 'LogDir' in code['config'].keys():
-        LogDir = os.path.abspath(code['config']['LogDir'])
-    else:
-        raise KeyError("Could not find LogDir in the YAML config section")
-
-    # Create directories if necessary
-    for d in [KernelDir, LogDir]:
-        if not os.path.isdir(d):
-            os.mkdir(d)
-
-    return code
-
-def dump_meas_file(data, filename = "", flatten=False):
-    d = Dumper if filename and not flatten else FlatDumper
-    d.add_representer(Include, d.include)
-
-    if filename:
-        with open(filename+".tmp", 'w+') as fid:
-
-            yaml.dump(data, fid, Dumper=d)
-        # Upon success
-        move(filename+".tmp", filename)
-        with open(filename, 'r') as fid:
-            contents = fid.read()
-        return contents
-    else:
-        # dump to an IO stream:
-        # note you need to use the FlatDumper for this to work
-        out = StringIO()
-        yaml.dump(data, out, Dumper=d)
-        ret_string = out.getvalue()
-        out.close()
-        return ret_string
