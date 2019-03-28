aa46b23d1a57bd57b8454ae5b89d6a3b15a583a0
tbrown <himorask@gmail.com>
2019-03-28 11:18:07 -0400

    Fix APS set channels
    set_amplitude accepts either a single channel or a channel pair as a 2-digit string --DR, LR

--- src/auspex/instruments/bbn.py
+++ src/auspex/instruments/bbn.py
@@ -229,12 +229,13 @@ class APS(Instrument, metaclass=MakeSettersGetters):
             self.connected = False
 
     def set_amplitude(self, chs, value):
+        import pdb; pdb.set_trace()
         if isinstance(chs, int) or len(chs)==1:
             self.wrapper.set_amplitude(int(chs), value)
         else:
             self.wrapper.set_amplitude(int(chs[0]), value)
-            self.wrapper.set_amplitude(int(chs[2]), value)
-            self.wrapper.set_amplitude(int(chs[3]), value)
+#            self.wrapper.set_amplitude(int(chs[2]), value)
+#            self.wrapper.set_amplitude(int(chs[3]), value)
             self.wrapper.set_amplitude(int(chs[1]), value)
 
     def set_offset(self, chs, value):
