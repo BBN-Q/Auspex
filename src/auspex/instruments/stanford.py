055f9d291b1768a67446d334a573a802937fa821
CoherenceController <qlab@bbn.com>
2019-05-21 16:46:20 -0400

    Added read_bytes to interface and fixed SRS driver

--- src/auspex/instruments/stanford.py
+++ src/auspex/instruments/stanford.py
@@ -84,7 +84,9 @@ class SR830(SCPIInstrument):
     def get_buffer(self, channel):
         stored_points = self.buffer_points
         self.interface.write("TRCB?{:d},0,{:d}".format(channel, stored_points))
-        buf = self.interface.read_raw()
+        #buf = self.interface.read_raw(numbytes=4)
+        buf = self.interface.read_bytes(4*stored_points,chunk_size=4)
+        logger.info(f"Raw buffer is {buf} with length {len(buf)} bytes.")
         return np.frombuffer(buf, dtype=np.float32)
 
     def buffer_start(self):
