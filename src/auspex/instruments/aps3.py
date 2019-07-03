299f05d869008fff12e2f3463035e06474963ae4
gribeill <guilhem.ribeill@gmail.com>
2019-07-02 23:09:40 -0400

    Add metaprogramming for bit-field commands

--- src/auspex/instruments/aps3.py
+++ src/auspex/instruments/aps3.py
@@ -6,20 +6,122 @@
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 
-__all__ = ['AMC599', 'APS3']
+#__all__ = ['AMC599', 'APS3']
 
-from .instrument import Instrument, MetaInstrument, is_valid_ipv4
+from .instrument import Instrument, is_valid_ipv4, Command
 from .bbn import MakeSettersGetters
 from auspex.log import logger
 import auspex.config as config
 from time import sleep
 import numpy as np
 import socket
+from unittest.mock import Mock
 import collections
 from struct import pack, iter_unpack
 
 U32 = 0xFFFFFFFF #mask for 32-bit unsigned int
 U16 = 0xFFFF
+
+def check_bits(value, shift, mask=0b1):
+    """Helper function to get a bit-slice of a 32-bit value.
+        Returns (value >> shift) & mask."""
+    return (((value & U32) >> shift) & mask)
+
+def set_bits(value, shift, x, mask=0b1):
+    """Set bits in a 32-bit value given an offset and a mask. """
+    return ((value & U32) & ~(mask << shift)) | ((x & U32) << shift)
+
+class BitFieldCommand(Command):
+    """An instrument command that sets/gets a value from a register.
+        See also the Command object in .instrument.py
+
+        Args:
+            None.
+        Kwargs:
+            register: Control register address. (required)
+            shift: 0-indexed bit position. (required)
+            mask: Size of bit field -- i.e. use 0b111 if setting a 3-bit value
+                defaults to 0b1 or is inferred from a value map.
+    """
+
+    def parse(self):
+        super().parse()
+
+        for a in ('register', 'shift', 'mask'):
+            if a in self.kwargs:
+                setattr(self, a, self.kwargs.pop(a))
+            else:
+                setattr(self, a, None)
+
+        if self.register is None:
+            raise ValueError("Must specify a destination or source register.")
+        if self.shift is None:
+            raise ValueError("Must specify a bit shift for register command.")
+
+        if self.mask is None:
+            if self.value_map is not None:
+                max_bits = max((v.bit_length() for v in self.value_map.items()))
+                self.mask = 2**max_bits - 1
+            else:
+                self.mask = 0b1
+
+    def convert_set(self, set_value_python):
+        if self.python_to_instr is None:
+            return int(set_value_python)
+        else:
+            return self.python_to_instr[set_value_python]
+
+    def convert_get(self, get_value_instrument):
+        if self.python_to_instr is None:
+            return bool(set_value_python)
+        else:
+            return self.instr_to_python[get_value_instrument]
+
+def add_command_bitfield(instr, name, cmd):
+    """Helper function to create a new BitFieldCommand when parsing an instrument."""
+
+    new_cmd = BitFieldCommand(*cmd.args, **cmd.kwargs)
+    new_cmd.parse()
+
+    def fget(self, **kwargs):
+        val = check_bits(self.read_register(new_cmd.register), new_cmd.shift, new_cmd.mask)
+        if new_cmd.get_delay is not None:
+            sleep(new_cmd.get_delay)
+        return new_cmd.convert_get(val)
+
+    def fset(self, val, **kwargs):
+        if new_cmd.value_range is not None:
+            if (val < new_cmd.value_range[0]) or (val > new_cmd.value_range[1]):
+                err_msg = "The value {} is outside of the allowable range {} specified for instrument '{}'.".format(val, new_cmd.value_range, self.name)
+                raise ValueError(err_msg)
+
+        if new_cmd.allowed_values is not None:
+            if not val in new_cmd.allowed_values:
+                err_msg = "The value {} is not in the allowable set of values specified for instrument '{}': {}".format(val, self.name, new_cmd.allowed_values)
+                raise ValueError(err_msg)
+
+        start_val = self.read_register(new_cmd.register)
+        new_val = set_bits(start_val, self.shift, new_cmd.convert_set(val), self.mask)
+        self.write_register(new_cmd.register, new_val)
+        if new_cmd.set_delay is not None:
+            sleep(new_cmd.set_delay)
+
+    setattr(instr, name, property(fget, fset, None, new_cmd.doc))
+
+    return new_cmd
+
+class MakeBitFieldParams(MakeSettersGetters):
+    def __init__(self, name, bases, dct):
+        super().__init__(self, name, bases, dct)
+
+        if 'write_register' not in dct or 'read_register' not in dct:
+            raise TypeError("An instrument using BitFieldParams must implement" +
+                " `read_register` and `write_register` functions.")
+
+        for k, v in dct.items():
+            if isinstance(v, BitFieldCommand):
+                logger.debug("Adding %s command", k)
+                nv = add_command_SCPI(self, k, v)
 
 class AMC599(object):
     """Base class for simple register manipulations of AMC599 board.
@@ -27,8 +129,11 @@ class AMC599(object):
 
     PORT = 0xbb4e # TCPIP port (BBN!)
 
-    def __init__(self):
+    def __init__(self, debug=False):
         self.connected = False
+        self.debug = debug
+        if self.debug:
+            self.debug_memory = {}
 
     def __del__(self):
         self.disconnect()
@@ -39,9 +144,10 @@ class AMC599(object):
 
     def connect(self, ip_addr="192.168.2.200"):
         self.ip_addr = ip_addr
-        self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-        self.socket.connect((self.ip_addr, self.PORT))
-        self.connected = True
+        if not self.debug:
+            self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+            self.socket.connect((self.ip_addr, self.PORT))
+            self.connected = True
 
     def disconnect(self):
         if self.connected:
@@ -57,20 +163,13 @@ class AMC599(object):
     def recv_bytes(self, size):
         resp = self.socket.recv(size)
         while True:
-            if len(ans) >= size:
+            if len(resp) >= size:
                 break
-            ans += self.socket.recv(8)
-        data = [x[0] for x in iter_unpack("!I", ans)]
+            resp += self.socket.recv(8)
+        data = [x[0] for x in iter_unpack("!I", resp)]
         return data if len(data)>1 else data[0]
 
     def write_memory(self, addr, data):
-        self._check_connected()
-        max_ct = 0xfffc #max writeable block length (TODO: check if still true)
-        cmd = 0x80000000 #write to RAM command
-        datagrams_written = 0
-        init_addr = addr
-        idx = 0
-
         if isinstance(data, int):
             data = [data]
         elif isinstance(data, list):
@@ -79,7 +178,20 @@ class AMC599(object):
         else:
             raise ValueError("Data must be an integer or a list of integers.")
 
-        data = [x & U32 for x in data] #make sure everything is 32 bits
+        if self.debug:
+            print("Wrote starting at memory address 0x%x:" % addr)
+            for d in data:
+                print(hex(d))
+            for off, d in enumerate(data):
+                self.debug_memory[addr + off*0x4] = d
+            return
+
+        self._check_connected()
+        max_ct = 0xfffc #max writeable block length (TODO: check if still true)
+        cmd = 0x80000000 #write to RAM command
+        datagrams_written = 0
+        init_addr = addr
+        idx = 0
 
         while (len(data) - idx > 0):
             ct_left = len(data) - idx
@@ -102,21 +214,23 @@ class AMC599(object):
             logger.debug("Wrote {} words in {} datagrams: {}", words_written,
                                 datagrams_written,
                                 [hex(x) for x in resp])
-            assert (results[2*ct] == 0x80800000 + words_written)
-            assert (results[2*ct+1] == addr)
+            assert (resp[2*ct] == 0x80800000 + words_written)
+            assert (resp[2*ct+1] == addr)
             addr += 4 * words_written
 
     def read_memory(self, addr, num_words):
+
+        if self.debug:
+            response = []
+            for x in range(num_words):
+                response.append(self.debug_memory.get(addr+0x4*x, 0x0))
+            return response[0] if num_words == 1 else response
+
         self._check_connected()
         datagram = [0x10000000 + num_words, addr]
         self.send_bytes(datagram)
         resp_header = self.recv_bytes(2 * 4) #4 bytes per word...
         return self.recv_bytes(4 * num_words)
-
-    def read_memory_hex(self, addr, num_words):
-        data = self.read_memory(addr, num_words)
-        for d in data:
-            print(hex(d))
 
 #####################################################################
 
@@ -125,7 +239,7 @@ class AMC599(object):
 #Registers are read/write unless otherwise noted
 #Current as of 6/20/19
 
-CSR_AXI_ADDR_BASE           = 0x44b4000
+CSR_AXI_ADDR_BASE           = 0x44b40000
 
 CSR_CACHE_CONTROL           = 0x0010 #Cache control register
 CSR_SEQ_CONTROL             = 0x0024 #Sequencer control register
@@ -176,13 +290,11 @@ DRAM_AXI_BASE = 0x80000000
 
 #####################################################################
 
-def check_bits(value, shift, mask=0b1):
-    return (((value & U32) >> shift) & mask)
 
-def set_bits(value, shift, x, mask=0b1):
-    return ((value & U32) & ~(mask << shift)) | ((x & U32) << shift)
 
 class APS3(Instrument):
+
+    instrument_type = "AWG"
 
     def __init__(self, resource_name=None, name="Unlabled APS3"):
         self.name = name
@@ -202,10 +314,11 @@ class APS3(Instrument):
         if self.board.connected:
             self.board.disconnect()
 
-    def write_csr(self, offset, data):
+    def write_register(self, offset, data):
+        logger.info(f"Setting CSR: {hex(offset)} to: {hex(data)}")
         self.board.write_memory(CSR_AXI_ADDR_BASE + offset, data)
 
-    def read_csr(self, offset, num_words = 1):
+    def read_register(self, offset, num_words = 1):
         return self.board.read_memory(CSR_AXI_ADDR_BASE + offset, num_words)
 
     def write_dram(self, offset, data):
@@ -289,7 +402,7 @@ class APS3(Instrument):
     @trigger_enable.setter
     def trigger_enable(self, value):
         reg = self.read_csr(CSR_SEQ_CONTROL)
-        self.write_csr(CSR_SEQ_CONTROL, set_bits(reg, 4, 0b1))
+        self.write_csr(CSR_SEQ_CONTROL, set_bits(reg, 4, int(value)))
 
     @property
     def bypass_modulator(self):
@@ -297,7 +410,7 @@ class APS3(Instrument):
     @bypass_modulator.setter
     def bypass_modulator(self, value):
         reg = self.read_csr(CSR_SEQ_CONTROL)
-        self.write_csr(CSR_SEQ_CONTROL, set_bits(reg, 5, 0b1))
+        self.write_csr(CSR_SEQ_CONTROL, set_bits(reg, 5, int(value)))
 
     @property
     def bypass_nco(self):
@@ -305,7 +418,7 @@ class APS3(Instrument):
     @bypass_nco.setter
     def bypass_nco(self, value):
         reg = self.read_csr(CSR_SEQ_CONTROL)
-        self.write_csr(CSR_SEQ_CONTROL, set_bits(reg, 6, 0b1))
+        self.write_csr(CSR_SEQ_CONTROL, set_bits(reg, 6, int(value)))
 
     ####### CORRECTION CONTROL REGISTER ########################################
     @property
@@ -346,7 +459,7 @@ class APS3(Instrument):
         commit_history = check_bits(fpga_rev, 16, 0x7FF)
         build_clean = "dirty" if check_bits(fpga_rev, 27, 0xF) else "clean"
 
-        return {"FPGA ID": fpga_id,
+        return {"FPGA ID": hex(fpga_id),
                 "FPGA REV": f"{fpga_build_major}.{fpga_build_minor} - {commit_history} - {build_clean}",
                 "GIT SHA1": hex(git_sha1),
                 "DATE": hex(build_tstamp)[2:]}
@@ -402,3 +515,48 @@ class APS3(Instrument):
         if value > U16:
             logger.warning(f"Marker delay {value} is greater than maximum allowed value of {U16} {hex(U16)}!")
         self.write_csr(CSR_MARKER_DELAY, value & U16)
+
+    ###### DRAM OFFSET REGISTERS ###############################################
+    @property
+    def SEQ_OFFSET(self):
+        return (self.read_csr(CSR_SEQ_OFFSET) - DRAM_AXI_BASE)
+
+    @property
+    def WFA_OFFSET(self):
+        return (self.read_csr(CSR_WFA_OFFSET) - DRAM_AXI_BASE)
+
+    @property
+    def WFB_OFFSET(self):
+        return (self.read_csr(CSR_WFB_OFFSET) - DRAM_AXI_BASE)
+
+    ###### UTILITIES ###########################################################
+    def run(self):
+        logger.info("Taking cache controller out of reset...")
+        self.cache_controller = True
+        sleep(0.01)
+        logger.info("Taking sequencer out of reset...")
+        self.sequencer_reset = True
+        sleep(0.01)
+        logger.info("Enabling trigger...")
+        self.trigger_enable = True
+        sleep(0.01)
+
+    def stop(self):
+        logger.info("Resetting cache controller...")
+        self.cache_controller = False
+        sleep(0.01)
+        logger.info("Resetting sequencer...")
+        self.sequencer_reset = False
+
+    def write_sequence(self, sequence):
+        packed_seq = []
+        for instr in sequence:
+            packed_seq.append(instr & U32)
+            packed_seq.append(instr >> 32)
+
+        self.cache_controller = False
+        sleep(0.01)
+        self.write_dram(self.SEQ_OFFSET, packed_seq)
+        logger.info(f"Wrote {len(packed_seq)} words to sequence memory.")
+        sleep(0.01)
+        self.cache_controller = True
