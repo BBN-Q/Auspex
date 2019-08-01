742c9b1520ea22ed201b07347fc40fdeb482036d
Guilhem Ribeill <guilhem.ribeill@gmail.com>
2019-08-01 15:21:37 -0400

    Don't complain about channel libraries without edges defined.

--- src/auspex/qubit/qubit_exp.py
+++ src/auspex/qubit/qubit_exp.py
@@ -120,7 +120,10 @@ class QubitExperiment(Experiment):
         self.controlled_qubits = [c for c in self.chan_db.channels if c.label in meta_info["qubits"]]
         self.measurements      = [c for c in self.chan_db.channels if c.label in meta_info["measurements"]]
         self.measured_qubits   = [c for c in self.chan_db.channels if "M-"+c.label in meta_info["measurements"]]
-        self.edges             = [c for c in self.chan_db.channels if c.label in meta_info["edges"]]
+        if 'edges' in meta_info:
+            self.edges             = [c for c in self.chan_db.channels if c.label in meta_info["edges"]]
+        else:
+            self.edges = []
         self.phys_chans        = list(set([e.phys_chan for e in self.controlled_qubits + self.measurements + self.edges]))
         self.transmitters      = list(set([e.phys_chan.transmitter for e in self.controlled_qubits + self.measurements + self.edges]))
         self.receiver_chans    = list(set([e.receiver_chan for e in self.measurements]))
