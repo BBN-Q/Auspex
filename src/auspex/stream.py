Fix buffer sizing for partial averages

src/auspex/stream.py
@@ -253,6 +253,12 @@ class DataStreamDescriptor(object):
         self.dtype = dtype
         self.metadata = {}
 
+        # Buffer size multiplier: use this to inflate the size of the 
+        # shared memory buffer. This is needed for partial averages, which
+        # may require more space than their descriptors would indicate
+        # since they are emitted as often as possible.
+        self.buffer_mult_factor = 1
+
         # Keep track of the parameter permutations we have actually used...
         self.visited_tuples = []
 
@@ -493,7 +499,7 @@ class DataStream(object):
         self.buff_idx       = Value('i', 0)
 
     def final_init(self):
-        self.buffer_size = self.descriptor.num_points()
+        self.buffer_size = self.descriptor.num_points()*self.descriptor.buffer_mult_factor
         # logger.info(f"{self.start_connector} to {self.end_connector} buffer of size {self.buffer_size}")
         if self.buffer_size > 50e6:
             logger.info("Limiting buffer size of {self} to 50 Million Points")
@@ -559,6 +565,9 @@ class DataStream(object):
         with self.buffer_lock:
             start = self.buff_idx.value
             re = np.real(data).flatten()
+            if start+re.size > self.re_np.size:
+                raise ValueError(f"Stream {self} received more data than fits in the shared memory buffer. \
+                    This is probably due to digitizer raw streams producing data too quickly for the pipeline.")
             self.re_np[start:start+re.size] = re
             if np.issubdtype(self.descriptor.dtype, np.complexfloating):
                 im = np.imag(data).flatten()