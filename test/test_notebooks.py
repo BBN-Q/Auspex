405785783cdf0cc81da5da7d7580f69f90ff5523
Matthew Ware <matt.ware@raytheon.com>
2019-09-17 15:49:54 -0400

    Don't repeat ourselves.  -MW

--- test/test_notebooks.py
+++ test/test_notebooks.py
@@ -1,3 +1,4 @@
+import glob
 from Notebook_testcase import NotebooksTestCase
 
 class NotebooksMethods(NotebooksTestCase):
@@ -14,30 +15,36 @@ class NotebooksMethods(NotebooksTestCase):
         auspex.config.KernelDir = kern_dir.name
 
 
-    def test_01_Essential_Objects(self):
-        self._setup()
-        self.runNotebook_jupyter("../doc/examples/","Example-Config.ipynb")
+    # def test_01_Essential_Objects(self):
+    #     self._setup()
+    #     self.runNotebook_jupyter("../doc/examples/","Example-Config.ipynb")
+    #
+    # def test_02_Essential_Objects(self):
+    #     self._setup()
+    #     self.runNotebook_jupyter("../doc/examples/","Example-Channel-Lib.ipynb")
+    #
+    # def test_03_Essential_Objects(self):
+    #     self._setup()
+    #     self.runNotebook_jupyter("../doc/examples/","Example-Calibrations.ipynb")
+    #
+    # def test_04_Essential_Objects(self):
+    #     self._setup()
+    #     self.runNotebook_jupyter("../doc/examples/","Example-Filter-Pipeline.ipynb")
+    #
+    # def test_05_Essential_Objects(self):
+    #     self._setup()
+    #     self.runNotebook_jupyter("../doc/examples/","Example-Experiments.ipynb")
+    #
+    # def test_06_Essential_Objects(self):
+    #     self._setup()
+    #     self.runNotebook_jupyter("../doc/examples/","Example-SingleShot-Fid.ipynb")
+    #
+    # def test_07_Essential_Objects(self):
+    #     self._setup()
+    #     self.runNotebook_jupyter("../doc/examples/","Example-Sweeps.ipynb")
 
-    def test_02_Essential_Objects(self):
-        self._setup()
-        self.runNotebook_jupyter("../doc/examples/","Example-Channel-Lib.ipynb")
-
-    def test_03_Essential_Objects(self):
-        self._setup()
-        self.runNotebook_jupyter("../doc/examples/","Example-Calibrations.ipynb")
-
-    def test_04_Essential_Objects(self):
-        self._setup()
-        self.runNotebook_jupyter("../doc/examples/","Example-Filter-Pipeline.ipynb")
-
-    def test_05_Essential_Objects(self):
-        self._setup()
-        self.runNotebook_jupyter("../doc/examples/","Example-Experiments.ipynb")
-
-    def test_06_Essential_Objects(self):
-        self._setup()
-        self.runNotebook_jupyter("../doc/examples/","Example-SingleShot-Fid.ipynb")
-
-    def test_07_Essential_Objects(self):
-        self._setup()
-        self.runNotebook_jupyter("../doc/examples/","Example-Sweeps.ipynb")
+    def test_All_Notebooks(self):
+        files = glob.glob('../doc/examples/*.ipynb')
+        for f in files:
+            self._setup()
+            self.runNotebook_jupyter(".", f)
