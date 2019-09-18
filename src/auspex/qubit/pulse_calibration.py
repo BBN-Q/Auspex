1548f576df83b6921d8a19c6bbee6d2a2af7d33c
gribeill <guilhem.ribeill@gmail.com>
2019-09-18 17:33:16 -0400

    Initial commit of CLEAR pulse calibration

--- src/auspex/qubit/pulse_calibration.py
+++ src/auspex/qubit/pulse_calibration.py
@@ -26,6 +26,7 @@ import subprocess
 import zmq
 import json
 import datetime
+from copy import copy
 
 import time
 import bbndb
@@ -1145,3 +1146,169 @@ def quick_norm_data(data): #TODO: generalize as in Qlab.jl
     data = 2*(data-np.mean(data[-4:-2]))/(np.mean(data[-4:-2])-np.mean(data[-2:])) + 1
     data = data[:-4]
     return data
+
+class CLEARCalibration(QubitCalibration):
+    '''Calibration of cavity reset pulse.
+
+    Args:
+        kappa: Cavity linewith (angular frequency: 1/s).
+        chi: Half of the dispersive shift (anguler frequency: 1/s).
+        t_empty: Time for active depletion (s).
+        alpha: Scaling factor.
+        T1factor: decay due to T1 between end of measurement and start of Ramsey.
+        T2: Measured T2*
+        nsteps: number of calibration steps
+        ramsey_delays: List of times to use for Ramsey experiment.
+        ramsey_freq: Ramsey offset frequency.
+        meas_delay: Delay after end of measurement pulse
+        preramsey_delay: Delay before start of Ramsey sequence.
+        eps1: 1st CLEAR parameter. if set to `None` will use theory values as default for eps1 and eps2.
+        eps2: 2nd CLEAR parameter.
+        cal_steps: Steps over which to sweep calibration.
+    '''
+
+    def __init__(self, qubit, kappa = 2*np.pi*2e6, chi = -2*np.pi*1e6, t_empty = 400e-9, 
+                ramsey_delays=np.linspace(0.0, 50.0, 51)*1e-6, ramsey_freq = 100e3, meas_delay = 0, 
+                preramsey_delay=0, alpha = 1, T1factor = 1, T2 = 30e-6, nsteps = 11, 
+                eps1 = None, eps2 = None, cal_steps = (1,1,1)):
+
+        self.kappa = kappa
+        self.chi = chi
+        self.ramsey_delays = ramsey_delays 
+        self.ramsey_freq = ramsey_freq 
+        self.meas_delay = meas_delay
+        self.preramsey_delay = preramsey_delay 
+        self.tau = t_empty/2.0
+        self.alpha = alpha 
+        self.T1factor = T1factor 
+        self.T2 = T2 
+        self.nsteps = nsteps 
+
+        #use theory values as defaults 
+        if not eps1: 
+            self.eps1 = ((1 - 2*np.exp(kappa*t_empty/4)*np.cos(chi*t_empty/2))
+                        /(1+np.exp(kappa*t_empty/2)-2*np.exp(kappa*t_empty/4)*np.cos(chi*t_empty/2)))
+            self.eps2 = 1/(1+np.exp(kappa*t_empty/2)-2*np.exp(kappa*t_empty/4)*np.cos(chi*t_empty/2))
+            logger.info(f' Using theoretical CLEAR amplitudes: {self.eps1} (eps1), {self.eps2} (eps2)')
+        else:
+            self.eps1 = eps1 
+            self.eps2 = eps2 
+
+        self.cal_steps = 1
+
+        self.seq_params = {}
+
+        super().__init__(qubit, **kwargs)
+        self.filename = 'CLEAR/CLEAR'
+
+    def descriptor(self):
+        return [delay_descriptor(self.delays)]
+
+    def sequence(self):
+        if self.seq_params['state']:
+            prep = X(self.qubit)
+        else:
+            prep = Id(self.qubit)
+
+        amp1 = self.alpha * self.seq_params['eps1']
+        amp2 = self.alpha * self.seq_params['eps2']
+
+
+        clear_meas = MEASCLEAR(self.qubit, amp1=amp1, amp2=amp2, step_length=self.seq_params['tau'])
+        seqs = [[prep, clear_meas, Id(self.qubit, self.preramsey_delay), X90(self.qubit), Id(self.qubit,d), 
+                    U90(self.qubit,phase = 2*pi*self.ramsey_freq*d), Id(self.qubit, self.meas_delay), MEAS(self.qubit)] 
+                        for d in self.ramsey_delays]
+
+        seqs += create_cal_seqs((self.qubit,), 2, delay = self.meas_delay)
+
+        return seqs
+
+    def init_plots(self):
+        self.plot_ramsey = ManualPlotter("CLEAR Ramsey", x_label='Time (us)', y_label='<Z>')
+        self.plot_clear = ManualPlotter("CLEAR Calibration", x_label=['eps1, eps2', 'eps1', 'eps2'], y_label=['Residual Photons'])
+
+        self.plot_ramsey.add_data_trace("Data")
+        self.plot_ramsey.add_fit_trace("Fit")
+
+        for sweep_num, state in product([0,1,2], [0,1]):
+            self.plot_clear.add_data_trace(f"Sweep {sweep_num}, State {state}", {"color": f'C{state+1}'}, sweep_num)
+            self.plot_clear.add_data_trace(f"Fit Sweep {sweep_num}, State {state}", {"color": f'C{state+1}'}, sweep_num)
+        
+        return [plot_ramsey, plot_clear]
+
+    def exp_config(self, exp):
+        pass #??
+        # rcvr = self.qubit.measure_chan.receiver_chan.receiver
+        # if self.first_ramsey:
+        #     if self.set_source:
+        #         self.source_proxy = self.qubit.phys_chan.generator # DB object
+        #         self.qubit_source = exp._instruments[self.source_proxy.label] # auspex instrument
+        #         self.orig_freq    = self.source_proxy.frequency
+        #         self.source_proxy.frequency = round(self.orig_freq + self.added_detuning, 10)
+        #         self.qubit_source.frequency = self.source_proxy.frequency
+        #     else:
+        #         self.orig_freq = self.qubit.frequency
+
+    def _calibrate(self):
+        
+        cal_step = 0
+        for ct in range(3):
+            if not self.cal_steps[ct]:
+                continue
+            xpoints = np.linspace(1-self.cal_steps[ct], 1+self.cal_steps[ct], self.nsteps)
+            n0vec   = np.zeros(self.nsteps)
+            err0vec = np.zeros(self.nsteps)
+            n1vec   = np.zeros(self.nsteps)
+            err1vec = np.zeros(self.nsteps)
+
+            self.seq_params = {'tau': self.tau}
+
+            for k in range(self.nsteps):
+                self.seq_params['eps1'] = self.eps1 if k == 1 else xpoints[k]*self.eps1
+                self.seq_params['eps1'] = self.eps2 if k == 2 else xpoints[k]*self.eps2
+                for state in [0,1]:
+                    self.seq_params['state'] = state 
+                    data, _ = self.run_sweeps()
+                    norm_data = quick_norm_data(data)
+
+                    fit = PhotonNumberFit(self.ramsey_delays, norm_data, [self.kappa, self.ramsey_freq, 2*self.chi, 
+                                                                          self.T2, self.T1fac, 0.0])
+
+                    if state == 0:
+                        n0vec[k] = fit.fit_params["Pb"]
+                        err0vec[k] = fit.fit_errors["Pb"]
+                    else:
+                        n1vec[k] = fit.fit_params["Pb"]
+                        err1vec[k] = fit.fit_errors["Pb"]
+                    cal_step +=1
+
+                    self.plot_ramsey["Data"] = (self.ramsey_delays, norm_data)
+                    self.plot_ramsey["Fit"] = (self.ramsey_delays, fit.model(self.ramsey_delays))
+                    self.plot_clear[f"Sweep {ct}, State 0"] = (xpoints, n0vec)
+                    self.plot_clear[f"Sweep {ct}, State 1"] = (xpoints, n1vec)
+
+            #Fit minimum photon number 
+            fit0 = QuadraticFit(xpoints, n0vec)
+            fit1 = QuadraticFit(xpoints, n1vec)
+            finer_xpoints = np.linspace(np.min(xpoints), np.max(xpoints), 4*len(xpoints))
+            self.plot_clear[f'Fit Sweep {ct}, State 0'] = (finer_xpoints, fit0.model(finer_xpoints))
+            self.plot_clear[f'Fit Sweep {ct}, State 0'] = (finer_xpoints, fit1.model(finer_xpoints))
+
+            opt_scaling = np.mean([fit0.fit_params["x0"], fit1.fit_params["x0"]])
+            logger.info(f"Optimal scaling factor for step {ct}: {opt_scaling}")
+
+            if ct<2:
+                self.eps1 *= opt_scaling
+            if ct != 1:
+                self.eps2 *= opt_scaling 
+
+
+
+        self.eps1 = round(float(self.eps1), 5)
+        self.eps2 = round(float(self.eps2), 5)
+        self.succeeded = True #TODO: add bounds
+
+    def update_settings(self):
+        self.qubit.measure_chan.pulse_params['amp1'] = self.eps1
+        self.qubit.measure_chan.pulse_params['amp2'] = self.eps2
+        self.qubit.measure_chan.pulse_params['step_length'] = round(float(self.tau), 9)
